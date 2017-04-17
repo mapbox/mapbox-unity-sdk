63e86892bd022917e52eb48de9c00455c12300ec
brnkhy <baran.kahyaoglu@mapbox.com>
2017-04-17 14:34:17 +0300

    add texture mod, uv mod, mod stack and merged mod stack comments

--- sdkproject/Assets/Mapbox/Core/Unity/MeshGeneration/Modifiers/GameObjectModifiers/TextureModifier.cs
+++ sdkproject/Assets/Mapbox/Core/Unity/MeshGeneration/Modifiers/GameObjectModifiers/TextureModifier.cs
@@ -3,6 +3,10 @@ namespace Mapbox.Unity.MeshGeneration.Modifiers
     using UnityEngine;
     using Mapbox.Unity.MeshGeneration.Components;
 
+    /// <summary>
+    /// Texture Modifier is a basic modifier which simply adds a TextureSelector script to the features.
+    /// Logic is all pushed into this TextureSelector mono behaviour to make it's easier to change it in runtime.
+    /// </summary>
     [CreateAssetMenu(menuName = "Mapbox/Modifiers/Texture Modifier")]
     public class TextureModifier : GameObjectModifier
     {
