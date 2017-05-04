7a94551c8422502fd025dbe20e8ccbb52ac77b17
bergwerkgis <wb@bergwerk-gis.at>
2017-05-04 13:34:48 +0200

    [wip] make UnityToolbag.Dispatcher work in Editor mode

--- sdkproject/Assets/ThirdPartyAssets/UnityToolbagDispatcher/Dispatcher.cs
+++ sdkproject/Assets/ThirdPartyAssets/UnityToolbagDispatcher/Dispatcher.cs
@@ -8,16 +8,25 @@ namespace UnityToolbag {
 	/// A system for dispatching code to execute on the main thread.
 	/// </summary>
 	[AddComponentMenu("UnityToolbag/Dispatcher")]
+	[ExecuteInEditMode]
+	//[UnityEditor.InitializeOnLoad]
 	public class Dispatcher : MonoBehaviour {
+
 		private static Dispatcher _instance;
 
 		// We can't use the behaviour reference from other threads, so we use a separate bool
 		// to track the instance so we can use that on the other threads.
 		private static bool _instanceExists;
 
+		private static bool _enabled;
+
 		private static Thread _mainThread;
 		private static object _lockObject = new object();
 		private static readonly Queue<Action> _actions = new Queue<Action>();
+
+		//public Dispatcher() {
+		//	UnityEngine.Debug.Log("Dispatcher()");
+		//}
 
 		/// <summary>
 		/// Gets a value indicating whether or not the current thread is the game's main thread.
@@ -34,6 +43,12 @@ namespace UnityToolbag {
 		/// <param name="action">The action to be queued.</param>
 		public static void InvokeAsync(Action action) {
 
+			UnityEngine.Debug.Log(string.Format(
+				"InvokeAsync, enabled:{0} instanceExists:{1} mainthread:{2}"
+				, _enabled
+				, _instanceExists
+				, null == _mainThread ? "NULL" : _mainThread.ManagedThreadId.ToString()
+			));
 			if (!_instanceExists) {
 				Debug.LogError("No Dispatcher exists in the scene. Actions will not be invoked!");
 				return;
@@ -56,11 +71,12 @@ namespace UnityToolbag {
 		/// <param name="action">The action to be queued.</param>
 		public static void Invoke(Action action) {
 
+			UnityEngine.Debug.Log(string.Format("Invoke, enabled:{0} instanceExists:{1}", _enabled, _instanceExists));
+
 			if (!_instanceExists) {
 				Debug.LogError("No Dispatcher exists in the scene. Actions will not be invoked!");
 				return;
 			}
-
 
 			bool hasRun = false;
 
@@ -80,34 +96,80 @@ namespace UnityToolbag {
 		}
 
 		void Awake() {
+			System.Diagnostics.StackTrace st = new System.Diagnostics.StackTrace(true);
+			System.Text.StringBuilder sb = new System.Text.StringBuilder();
+			for (int i = 0; i < st.FrameCount; i++) {
+				System.Diagnostics.StackFrame sf = st.GetFrame(i);
+				sb.AppendLine(sf.GetMethod().ToString() + " " + sf.GetFileName() + " " + sf.GetFileLineNumber() + " " + sf.GetFileColumnNumber());
+			}
+			UnityEngine.Debug.Log("Awake()" + Environment.NewLine + sb.ToString());
+
+#if UNITY_EDITOR
+			if (Application.isEditor && !Application.isPlaying) {
+				UnityEngine.Debug.Log("Awake(), hooking into EditorUpdate");
+				UnityEditor.EditorApplication.update += EditorUpdate;
+			}
+#endif
 			if (_instance) {
+				UnityEngine.Debug.Log("Awake() destroying instance");
 				DestroyImmediate(this);
 			} else {
+				UnityEngine.Debug.Log("Awake() creating instance");
 				_instance = this;
+				//_instance = Mapbox.Unity.Utilities.Singleton<Dispatcher>.Instance;
 				_instanceExists = true;
 				_mainThread = Thread.CurrentThread;
 			}
+
+			UnityEngine.Debug.Log(string.Format(
+				"Awake(), enabled:{0} instanceExists:{1} mainthread:{2}"
+				, _enabled
+				, _instanceExists
+				, null == _mainThread ? "NULL" : _mainThread.ManagedThreadId.ToString()
+			));
 		}
 
+
 		void OnDestroy() {
+			UnityEngine.Debug.Log("OnDestroy()");
+#if UNITY_EDITOR
+			if (Application.isEditor && !Application.isPlaying) {
+				UnityEngine.Debug.Log("Awake(), disconnecting EditorUpdate");
+				UnityEditor.EditorApplication.update -= EditorUpdate;
+			}
+#endif
 			if (_instance == this) {
 				_instance = null;
 				_instanceExists = false;
 			}
 		}
 
-		//int cnt = 0;
-		void Update() {
-			//cnt++;
-			//if (cnt < 100) { return; }
-			//cnt = 0;
-			////if (_actions.Count > 0) UnityEngine.Debug.Log("-------------------- update -----------------" + DateTime.Now.Ticks);
+
+		private void Update() {
+			UnityEngine.Debug.Log(string.Format("Update(), enabled:{0} instanceExists:{1}", _enabled, _instanceExists));
+			MyUpdate();
+		}
+
+
+		private void EditorUpdate() {
+			UnityEngine.Debug.Log(string.Format("EditorUpdate(), enabled:{0} instanceExists:{1}", _enabled, _instanceExists));
+			MyUpdate();
+		}
+
+		void MyUpdate() {
 			lock (_lockObject) {
 				while (_actions.Count > 0) {
-					//UnityEngine.Debug.Log("QUEUE: " + _actions.Count + " " + DateTime.Now.Ticks);
 					_actions.Dequeue()();
 				}
 			}
 		}
+
+
+		//private void OnEnable() {
+		//	UnityEngine.Debug.Log("======== OnEnable() ==========");
+		//	_enabled = true;
+		//}
+
+
 	}
 }
