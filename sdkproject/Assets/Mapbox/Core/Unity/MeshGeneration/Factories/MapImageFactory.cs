526e104fcf978e9a60675ddf62904b3626babd33
bergwerkgis <wb@bergwerk-gis.at>
2017-05-09 13:50:43 +0200

    * update to latest mapbox-sdk-cs@IAsyncRequestFactory * changes to account for latest mapbox-sdk-cs

--- sdkproject/Assets/Mapbox/Core/Unity/MeshGeneration/Factories/MapImageFactory.cs
+++ sdkproject/Assets/Mapbox/Core/Unity/MeshGeneration/Factories/MapImageFactory.cs
@@ -1,117 +1,101 @@
 using Mapbox.Unity.Utilities;
-namespace Mapbox.Unity.MeshGeneration.Factories
-{
-    using System;
-    using System.Collections.Generic;
-    using Mapbox.Map;
-    using UnityEngine;
-    using Mapbox.Unity.MeshGeneration.Enums;
-    using Mapbox.Unity.MeshGeneration.Data;
-    using Mapbox.Platform;
+namespace Mapbox.Unity.MeshGeneration.Factories {
+	using System;
+	using System.Collections.Generic;
+	using Mapbox.Map;
+	using UnityEngine;
+	using Mapbox.Unity.MeshGeneration.Enums;
+	using Mapbox.Unity.MeshGeneration.Data;
+	using Mapbox.Platform;
 
-    public enum MapImageType
-    {
-        BasicMapboxStyle,
-        Custom,
-        None
-    }
+	public enum MapImageType {
+		BasicMapboxStyle,
+		Custom,
+		None
+	}
 
-    /// <summary>
-    /// Uses raster image services to create materials & textures for terrain
-    /// </summary>
-    [CreateAssetMenu(menuName = "Mapbox/Factories/Map Image Factory")]
-    public class MapImageFactory : Factory
-    {
-        [SerializeField]
-        private MapImageType _mapIdType;
-        [SerializeField]
-        private string _customMapId = "";
-        [SerializeField]
-        private string _mapId = "";
-        [SerializeField]
-        public Material _baseMaterial;
+	/// <summary>
+	/// Uses raster image services to create materials & textures for terrain
+	/// </summary>
+	[CreateAssetMenu(menuName = "Mapbox/Factories/Map Image Factory")]
+	public class MapImageFactory : Factory {
+		[SerializeField]
+		private MapImageType _mapIdType;
+		[SerializeField]
+		private string _customMapId = "";
+		[SerializeField]
+		private string _mapId = "";
+		[SerializeField]
+		public Material _baseMaterial;
 
-        [SerializeField]
-        TextureFormat _textureFormat = TextureFormat.DXT1;
+		[SerializeField]
+		TextureFormat _textureFormat = TextureFormat.DXT1;
 
-        [SerializeField]
-        bool _useMipMap = false;
+		[SerializeField]
+		bool _useMipMap = false;
 
-        [SerializeField]
-        bool _useRetina;
+		[SerializeField]
+		bool _useRetina;
 
-        private Dictionary<Vector2, UnityTile> _tiles;
+		private Dictionary<Vector2, UnityTile> _tiles;
 
-        public override void Initialize(IFileSource fs)
-        {
-            base.Initialize(fs);
-            _tiles = new Dictionary<Vector2, UnityTile>();
-        }
+		public override void Initialize(IFileSource fs) {
+			base.Initialize(fs);
+			_tiles = new Dictionary<Vector2, UnityTile>();
+		}
 
-        public override void Register(UnityTile tile)
-        {
-            base.Register(tile);
-            _tiles.Add(tile.TileCoordinate, tile);
-            Run(tile);
-        }
+		public override void Register(UnityTile tile) {
+			base.Register(tile);
+			_tiles.Add(tile.TileCoordinate, tile);
+			Run(tile);
+		}
 
-        public override void Update()
-        {
-            base.Update();
-            foreach (var tile in _tiles.Values)
-            {
-                Run(tile);
-            }
-        }
+		public override void Update() {
+			base.Update();
+			foreach (var tile in _tiles.Values) {
+				Run(tile);
+			}
+		}
 
-        /// <summary>
-        /// Fetches the image and applies it to tile material.
-        /// MapImage factory currently supports both new (RasterTile) and classic (ClassicRasterTile) Mapbox styles.
-        /// </summary>
-        /// <param name="tile"></param>
-        private void Run(UnityTile tile)
-        {
-            if (!string.IsNullOrEmpty(_mapId))
-            {
-                var parameters = new Tile.Parameters();
-                parameters.Fs = this.FileSource;
-                parameters.Id = new CanonicalTileId(tile.Zoom, (int)tile.TileCoordinate.x, (int)tile.TileCoordinate.y);
-                parameters.MapId = _mapId;
+		/// <summary>
+		/// Fetches the image and applies it to tile material.
+		/// MapImage factory currently supports both new (RasterTile) and classic (ClassicRasterTile) Mapbox styles.
+		/// </summary>
+		/// <param name="tile"></param>
+		private void Run(UnityTile tile) {
+			if (!string.IsNullOrEmpty(_mapId)) {
+				var parameters = new Tile.Parameters();
+				parameters.Fs = this.FileSource;
+				parameters.Id = new CanonicalTileId(tile.Zoom, (int)tile.TileCoordinate.x, (int)tile.TileCoordinate.y);
+				parameters.MapId = _mapId;
 
-                tile.ImageDataState = TilePropertyState.Loading;
+				tile.ImageDataState = TilePropertyState.Loading;
 
-                RasterTile rasterTile;
-                if (parameters.MapId.StartsWith("mapbox://", StringComparison.Ordinal))
-                {
-                    rasterTile = _useRetina ? new RetinaRasterTile() : new RasterTile();
-                }
-                else 
-                {
-                    rasterTile = _useRetina ? new ClassicRetinaRasterTile() : new ClassicRasterTile();
-                }
+				RasterTile rasterTile;
+				if (parameters.MapId.StartsWith("mapbox://", StringComparison.Ordinal)) {
+					rasterTile = _useRetina ? new RetinaRasterTile() : new RasterTile();
+				} else {
+					rasterTile = _useRetina ? new ClassicRetinaRasterTile() : new ClassicRasterTile();
+				}
 
-                rasterTile.Initialize(parameters, (Action)(() =>
-                {
-                    if (rasterTile.Error != null)
-                    {
-                        tile.ImageDataState = TilePropertyState.Error;
-                        return;
-                    }
+				rasterTile.Initialize(parameters, (Action)(() => {
+					if (rasterTile.HasError) {
+						tile.ImageDataState = TilePropertyState.Error;
+						return;
+					}
 
-                    var rend = tile.GetComponent<MeshRenderer>();
-                    rend.material = _baseMaterial;
-                    tile.ImageData = new Texture2D(0, 0, _textureFormat, _useMipMap);
-                    tile.ImageData.LoadImage(rasterTile.Data);
-                    rend.material.mainTexture = tile.ImageData;
-                    tile.ImageDataState = TilePropertyState.Loaded;
+					var rend = tile.GetComponent<MeshRenderer>();
+					rend.material = _baseMaterial;
+					tile.ImageData = new Texture2D(0, 0, _textureFormat, _useMipMap);
+					tile.ImageData.LoadImage(rasterTile.Data);
+					rend.material.mainTexture = tile.ImageData;
+					tile.ImageDataState = TilePropertyState.Loaded;
 
-                }));
-            }
-            else
-            {
-                var rend = tile.GetComponent<MeshRenderer>();
-                rend.material = _baseMaterial;
-            }
-        }
-    }
+				}));
+			} else {
+				var rend = tile.GetComponent<MeshRenderer>();
+				rend.material = _baseMaterial;
+			}
+		}
+	}
 }
--- sdkproject/Assets/Mapbox/Core/Unity/Utilities/HTTPRequest.cs
+++ sdkproject/Assets/Mapbox/Core/Unity/Utilities/HTTPRequest.cs
@@ -4,58 +4,62 @@
 // </copyright>
 //-----------------------------------------------------------------------
 
-namespace Mapbox.Unity.Utilities
-{
-    using System;
-    using UnityEngine.Networking;
-    using System.Collections;
-    using Mapbox.Platform;
+
+namespace Mapbox.Unity.Utilities {
+
+	using System;
+	using UnityEngine.Networking;
+	using System.Collections;
+	using Mapbox.Platform;
 
 #if UNITY_EDITOR
-    using UnityEditor;
+	using UnityEditor;
 #endif
 
-    internal sealed class HTTPRequest : IAsyncRequest
-    {
-        private UnityWebRequest _request;
-        private readonly Action<Response> _callback;
+	internal sealed class HTTPRequest : IAsyncRequest {
 
-        public HTTPRequest(string url, Action<Response> callback)
-        {
-            _request = UnityWebRequest.Get(url);
-            _callback = callback;
+		private UnityWebRequest _request;
+		private int _timeout;
+		private readonly Action<Response> _callback;
+
+		public bool IsCompleted { get; private set; }
+
+		public HTTPRequest(string url, Action<Response> callback, int timeout = 10) {
+			IsCompleted = false;
+			_timeout = timeout;
+			_request = UnityWebRequest.Get(url);
+			_callback = callback;
 
 #if UNITY_EDITOR
-            if (!EditorApplication.isPlaying)
-            {
-                Runnable.EnableRunnableInEditor();
-            }
+			if (!EditorApplication.isPlaying) {
+				Runnable.EnableRunnableInEditor();
+			}
 #endif
-            Runnable.Run(DoRequest());
-        }
+			Runnable.Run(DoRequest());
+		}
 
-        public void Cancel()
-        {
-            if (_request != null)
-            {
-                _request.Abort();
-            }
-        }
+		public void Cancel() {
+			if (_request != null) {
+				_request.Abort();
+			}
+		}
 
-        private IEnumerator DoRequest()
-        {
-            _request.Send();
-            while (!_request.isDone)
-            {
-                yield return 0;
-            }
-            var response = new Response();
-            response.Error = this._request.error;
-            response.Data = this._request.downloadHandler.data;
+		private IEnumerator DoRequest() {
+			_request.Send();
+			while (!_request.isDone) {
+				yield return 0;
+			}
+			var response = new Response();
+			// TODO: evalute _request more thoroughly and set properties of Response accordingly: status code, headers, etc.
+			if (!string.IsNullOrEmpty(_request.error)) {
+				response.AddException(new Exception(_request.error));
+			}
+			response.Data = this._request.downloadHandler.data;
 
-            _callback(response);
-            _request.Dispose();
-            _request = null;
-        }
-    }
+			_callback(response);
+			_request.Dispose();
+			_request = null;
+			IsCompleted = true;
+		}
+	}
 }
