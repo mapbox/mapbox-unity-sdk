660c7cb694e61f733fd21f288b3458eca1df1bf2
brnkhy <baran.kahyaoglu@mapbox.com>
2017-05-16 20:45:43 +0300

    fix using statement after submodule update

--- sdkproject/Assets/Mapbox/Core/Unity/MeshGeneration/Modifiers/MeshModifiers/ClipperLineMeshModifier.cs
+++ sdkproject/Assets/Mapbox/Core/Unity/MeshGeneration/Modifiers/MeshModifiers/ClipperLineMeshModifier.cs
@@ -5,7 +5,7 @@ namespace Mapbox.Unity.MeshGeneration.Modifiers
     using UnityEngine;
     using Mapbox.Unity.MeshGeneration.Data;
     using TriangleNet.Geometry;
-    using Mapbox.VectorTile.InteralClipperLib;
+    using Mapbox.VectorTile.Geometry.InteralClipperLib;
 
     [CreateAssetMenu(menuName = "Mapbox/Modifiers/Clipper Line Mesh Modifier")]
     public class ClipperLineMeshModifier : MeshModifier
