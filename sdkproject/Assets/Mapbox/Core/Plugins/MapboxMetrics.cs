297dbe3bd1d7946cbb87532ef39364afe88c4b1c
Siyu Song <siyu.song@gmail.com>
2017-05-20 23:47:37 -0700

    scrubbing token in favor of mapboxaccess token read

--- sdkproject/Assets/Mapbox/Core/Plugins/MapboxMetrics.cs
+++ sdkproject/Assets/Mapbox/Core/Plugins/MapboxMetrics.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using UnityEngine;
+using Mapbox.Unity;
 
 /*
   Manually set Xcode deployment target to 8.0 (needs to be 8+)
@@ -23,13 +24,13 @@ public class MapboxMetrics : MonoBehaviour
 	// Use this for initialization
 	void Start () {
 		Debug.Log("starting mapboxmetrics..");
-
+		Debug.Log (MapboxAccess.Instance.AccessToken);
 		#if UNITY_IOS && !UNITY_EDITOR
 		Debug.Log("starting mapboxmetrics.. ios");
 		// This gives the telem library enough information to operate. It must be called before any other methods
 		// In a real app, the game developers public key should be used and we still need to figure out what the
 		// actual value for "MapboxEventsUnityiOS" should be
-		initialize("pk.eyJ1IjoiYm91bmRzaiIsImEiOiJjajJ2Njk0djcwMDRsMzhxcDQ1a21zeGgyIn0.bzFNX9kJ5QQilSeuDenZdA", "MapboxEventsUnityiOS");
+		initialize( MapboxAccess.Instance.AccessToken , "MapboxEventsUnityiOS");
 
 		// This tells the telem library to send a turnstile event. It can be called as often as you want but ideally only when 
 		// there is a relevant event that our Unity SDK knows about (i.e. a map load or game session start)
