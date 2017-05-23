582d0f3bf633e67a17174c742c0b82165ce33492
Wilhelm Berg <BergWerkGIS@users.noreply.github.com>
2017-05-23 18:17:11 +0200

    #if UNITY_IOS (#82)

--- sdkproject/Assets/Mapbox/Unity/Editor/Build/Mapbox_iOS_build.cs
+++ sdkproject/Assets/Mapbox/Unity/Editor/Build/Mapbox_iOS_build.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿#if UNITY_IOS
+using UnityEngine;
 using UnityEditor;
 using UnityEditor.Callbacks;
 using UnityEditor.iOS.Xcode;
@@ -26,3 +27,4 @@ public class Mapbox_iOS_build : MonoBehaviour
 		}
 	}
 }
+#endif
