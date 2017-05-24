3aac755f4a83d1e6c040411df06fb8b31d32b405
bergwerkgis <wb@bergwerk-gis.at>
2017-05-24 15:24:01 +0200

    no error from `new AndroidJavaClass("com.mapbox.services.android.telemetry.MapboxTelemetry");`

---
+++ sdkproject/Assets/Mapbox/Examples/MapboxMetricsTestAndroid/LoadAndroidAAR.cs
@@ -0,0 +1,27 @@
+﻿#if UNITY_ANDROID
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadAndroidAAR : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start()
+	{
+		Debug.Log("=======================");
+		Debug.Log("=======================");
+		Debug.Log("=======================");
+		Debug.Log("======================= BEFORE AndroidJavaClass");
+		var MapboxAndroidTelem = new AndroidJavaClass("com.mapbox.services.android.telemetry.MapboxTelemetry");
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+}
+
+#endif
