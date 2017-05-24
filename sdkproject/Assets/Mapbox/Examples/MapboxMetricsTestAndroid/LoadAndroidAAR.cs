101c378ab6cf815a7d9ebfa4fa97f4fb1164a5f7
bergwerkgis <wb@bergwerk-gis.at>
2017-05-24 15:55:09 +0200

    telem is initializing: AndroidManifest.xml error tough

--- sdkproject/Assets/Mapbox/Examples/MapboxMetricsTestAndroid/LoadAndroidAAR.cs
+++ sdkproject/Assets/Mapbox/Examples/MapboxMetricsTestAndroid/LoadAndroidAAR.cs
@@ -6,14 +6,45 @@ using UnityEngine;
 public class LoadAndroidAAR : MonoBehaviour
 {
 
-	// Use this for initialization
+	private AndroidJavaObject _activityContext = null;
+	private AndroidJavaObject _telemInstance = null;
+
 	void Start()
 	{
-		Debug.Log("=======================");
-		Debug.Log("=======================");
-		Debug.Log("=======================");
-		Debug.Log("======================= BEFORE AndroidJavaClass");
-		var MapboxAndroidTelem = new AndroidJavaClass("com.mapbox.services.android.telemetry.MapboxTelemetry");
+		if (null != _telemInstance) { return; }
+
+		using (AndroidJavaClass activityClass = new AndroidJavaClass("com.unity3d.player.UnityPlayer"))
+		{
+			_activityContext = activityClass.GetStatic<AndroidJavaObject>("currentActivity");
+		}
+
+		Debug.Log("======================= BEFORE new AndroidJavaClass =======================");
+		using (AndroidJavaClass MapboxAndroidTelem = new AndroidJavaClass("com.mapbox.services.android.telemetry.MapboxTelemetry"))
+		{
+			Debug.Log("======================= AFTER new AndroidJavaClass =======================");
+			if (null == MapboxAndroidTelem)
+			{
+				Debug.LogError("====== could not get class 'MapboxTelemetry'");
+				return;
+			}
+
+			Debug.Log("======================= before CallStatic<AndroidJavaObject>('getInstance') =======================");
+			_telemInstance = MapboxAndroidTelem.CallStatic<AndroidJavaObject>("getInstance");
+			Debug.Log("======================= after CallStatic<AndroidJavaObject>('getInstance') =======================");
+
+			Debug.Log("null==_telemInstance:" + (null == _telemInstance));
+
+			Debug.Log("======================= before _telemInstance.Call('initialize' =======================");
+			_telemInstance.Call(
+				"initialize"
+				, _activityContext
+				, Mapbox.Unity.MapboxAccess.Instance.AccessToken
+				, "unity-sdk-android"
+			);
+			Debug.Log("======================= after _telemInstance.Call('initialize' =======================");
+
+
+		}
 
 	}
 
