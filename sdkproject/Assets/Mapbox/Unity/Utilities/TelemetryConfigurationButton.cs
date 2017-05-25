44cc2d6e5ccaaece3b843ea63fb27a95924c0152
David Rhodes <david.rhodes@mapbox.com>
2017-05-25 16:06:34 -0600

    Attribution updates (#90)
    * initial overhaul of attribution prefab
    
    * Packing sprites.
    Popups and web links.
    Telemetry opt in/out stub
    
    * More precise button colliders.
    Adding attribution to example scenes.
    
    * Removed duplicate event system.
    
    * Nothing to see here, PV
    
    * Added attribution prefab to these examples
    
    * Fonts are no longer nested
    
    * Stubbing in storage setting for telemetry configuration.

---
+++ sdkproject/Assets/Mapbox/Unity/Utilities/TelemetryConfigurationButton.cs
@@ -0,0 +1,23 @@
+﻿namespace Mapbox.Unity.Utilities
+{
+	using UnityEngine;
+	using UnityEngine.UI;
+
+	[RequireComponent(typeof(Button))]
+	public class TelemetryConfigurationButton : MonoBehaviour
+	{
+		[SerializeField]
+		bool _booleanValue;
+
+		void Awake()
+		{
+			GetComponent<Button>().onClick.AddListener(SetPlayerPref);
+		}
+
+		void SetPlayerPref()
+		{
+			PlayerPrefs.SetInt(Constants.Path.IS_TELEMETRY_ENABLED_KEY, (_booleanValue ? 1 : 0));
+			PlayerPrefs.Save();
+		}
+	}
+}
