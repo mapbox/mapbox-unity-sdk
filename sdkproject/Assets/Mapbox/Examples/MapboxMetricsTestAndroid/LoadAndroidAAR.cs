ea3b5740ac82be873f8b3b3436cf9f339dd882f0
bergwerkgis <wb@bergwerk-gis.at>
2017-05-25 14:19:42 +0200

    Telemetry [wip] Refactor a bit an implement other events

--- sdkproject/Assets/Mapbox/Examples/MapboxMetricsTestAndroid/LoadAndroidAAR.cs
+++ sdkproject/Assets/Mapbox/Examples/MapboxMetricsTestAndroid/LoadAndroidAAR.cs
@@ -8,76 +8,112 @@ public class LoadAndroidAAR : MonoBehaviour
 
 	private AndroidJavaObject _activityContext = null;
 	private AndroidJavaObject _telemInstance = null;
-	private AndroidJavaObject _mapLoadEvent = null;
 
 	void Start()
 	{
+		TelemetryInitialize();
+		TelemetryPushMapLoadEvent();
+	}
+
+
+	int _previousSecond=-1;
+	void Update()
+	{
+		// JUST FOR TESTING
+		// FIRE EVENTS EVERY FEW SECONDS
+		int second = System.DateTime.Now.Second;
+		if (_previousSecond == second) { return; }
+		_previousSecond = second;
+
+		if (0 == second % 21) { TelemetryPushMapCllickEvent(); }
+		if (0 == second % 27) { TelemetryPushMapDragEndEvent(); }
+	}
+
+
+	void TelemetryInitialize()
+	{
+		// Already initialized
 		if (null != _telemInstance) { return; }
 
-		#if UNITY_ANDROID && !UNITY_EDITOR
+#if UNITY_ANDROID && !UNITY_EDITOR
 		using (AndroidJavaClass activityClass = new AndroidJavaClass("com.unity3d.player.UnityPlayer"))
 		{
 			_activityContext = activityClass.GetStatic<AndroidJavaObject>("currentActivity");
 		}
 
-		Debug.Log("======================= BEFORE new AndroidJavaClass =======================");
+		if (null == _activityContext)
+		{
+			Debug.LogError("Could not get current activity");
+			return;
+		}
+
 		using (AndroidJavaClass MapboxAndroidTelem = new AndroidJavaClass("com.mapbox.services.android.telemetry.MapboxTelemetry"))
 		{
-			Debug.Log("======================= AFTER new AndroidJavaClass =======================");
 			if (null == MapboxAndroidTelem)
 			{
-				Debug.LogError("====== could not get class 'MapboxTelemetry'");
+				Debug.LogError("Could not get class 'MapboxTelemetry'");
 				return;
 			}
 
-			Debug.Log("======================= before CallStatic<AndroidJavaObject>('getInstance') =======================");
 			_telemInstance = MapboxAndroidTelem.CallStatic<AndroidJavaObject>("getInstance");
-			Debug.Log("======================= after CallStatic<AndroidJavaObject>('getInstance') =======================");
+			if (null == _telemInstance)
+			{
+				Debug.LogError("Could not get MapboxTelemetry instance");
+				return;
+			}
 
-			Debug.Log("null==_telemInstance:" + (null == _telemInstance));
-
-			Debug.Log("======================= before _telemInstance.Call('initialize' =======================");
 			_telemInstance.Call(
 				"initialize"
 				, _activityContext
 				, Mapbox.Unity.MapboxAccess.Instance.AccessToken
 				, "MapboxEventsUnityAndroid"
 			);
-			Debug.Log("======================= after _telemInstance.Call('initialize' =======================");
-
-			// MapboxTelemetry.getInstance().pushEvent(MapboxEvent.buildMapLoadEvent());
-			//_telemInstance.Call
-
-			Debug.Log("======================= get mapbox event call =======================");
-
-			///Hashtable<String, Object> event = MapboxEvent.buildMapLoadEvent();
-
-			using (AndroidJavaClass MapboxAndroidEvent= new AndroidJavaClass("com.mapbox.services.android.telemetry.MapboxEvent"))
-			{
-				if (null == MapboxAndroidEvent)
-				{
-					Debug.LogError("====== could not get class 'MapboxEvent'");
-					return;
-				}
-
-				Debug.Log("======================= before MapboxAndroidEvent CallStatic<AndroidJavaObject>('buildMapLoadEvent') =======================");
-				_mapLoadEvent = MapboxAndroidEvent.CallStatic<AndroidJavaObject>("buildMapLoadEvent");
-				Debug.Log("======================= after MapboxAndroidEvent CallStatic<AndroidJavaObject>('buildMapLoadEvent') =======================");
-
-				_telemInstance.Call ("pushEvent", _mapLoadEvent);
-
-				Debug.Log("======================= after _telemInstance Call<AndroidJavaObject>('pushEvent') =======================");
-			}
-				
-			Debug.Log("======================= after _telemInstance.Call('pushEvent' =======================");
 		}
-		#endif
+#endif
 	}
 
-	// Update is called once per frame
-	void Update()
+
+	void TelemetryPushMapLoadEvent()
 	{
+		if (null == _telemInstance) { return; }
+
+		using (AndroidJavaClass MapboxAndroidEvent = new AndroidJavaClass("com.mapbox.services.android.telemetry.MapboxEvent"))
+		{
+			if (null == MapboxAndroidEvent)
+			{
+				Debug.LogError("Could not get class 'MapboxEvent'");
+				return;
+			}
+
+			AndroidJavaObject mapLoadEvent = MapboxAndroidEvent.CallStatic<AndroidJavaObject>("buildMapLoadEvent");
+			_telemInstance.Call("pushEvent", mapLoadEvent);
+		}
 
 	}
+
+
+	void TelemetryPushMapCllickEvent()
+	{
+		Debug.Log("======== TelemetryPushMapCllickEvent() ========");
+		if (null == _telemInstance)
+		{
+			Debug.LogError("Telemetry not initialized");
+			return;
+		}
+	}
+
+
+	void TelemetryPushMapDragEndEvent()
+	{
+		Debug.Log("======== TelemetryPushMapDragEnd() ========");
+		if (null == _telemInstance)
+		{
+			Debug.LogError("Telemetry not initialized");
+			return;
+		}
+
+	}
+
+
 }
 
