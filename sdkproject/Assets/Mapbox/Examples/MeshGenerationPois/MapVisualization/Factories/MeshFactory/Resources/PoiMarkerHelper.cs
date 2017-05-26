dc5b7cd26e6234c250a3e2962195699d169e5e41
David Rhodes <david.rhodes@mapbox.com>
2017-05-26 09:15:46 -0600

    Removed duplicate event system. Print poi information on click of 3D poi (show that this is dynamic).

--- sdkproject/Assets/Mapbox/Examples/MeshGenerationPois/MapVisualization/Factories/MeshFactory/Resources/PoiMarkerHelper.cs
+++ sdkproject/Assets/Mapbox/Examples/MeshGenerationPois/MapVisualization/Factories/MeshFactory/Resources/PoiMarkerHelper.cs
@@ -1,13 +1,21 @@
 ﻿using UnityEngine;
-using System.Collections;
 using Mapbox.Unity.MeshGeneration.Interfaces;
-using System;
 using System.Collections.Generic;
 
 public class PoiMarkerHelper : MonoBehaviour, IFeaturePropertySettable
 {
-    public void Set(Dictionary<string, object> props)
-    {
-        
-    }
+	Dictionary<string, object> _props;
+
+	public void Set(Dictionary<string, object> props)
+	{
+		_props = props;
+	}
+
+	void OnMouseUpAsButton()
+	{
+		foreach (var prop in _props)
+		{
+			Debug.Log(prop.Key + ":" + prop.Value);
+		}
+	}
 }
