4f7fc17f817b2d1d04ab92dadda6f5195f38aca0
BergWerkGIS <wb@BergWerk-GIS.at>
2017-06-16 12:26:20 +0200

    fix Voxel demo to work with MbTilesCaching

--- sdkproject/Assets/Mapbox/Examples/Voxels/Scripts/VoxelTile.cs
+++ sdkproject/Assets/Mapbox/Examples/Voxels/Scripts/VoxelTile.cs
@@ -1,4 +1,5 @@
-namespace Mapbox.Examples.Voxels {
+namespace Mapbox.Examples.Voxels
+{
 	using Mapbox.Map;
 	using Mapbox.Unity;
 	using System.Collections.Generic;
@@ -9,7 +10,8 @@ namespace Mapbox.Examples.Voxels {
 	using Mapbox.Utils;
 	using Mapbox.Platform;
 	using Mapbox.Unity.Utilities;
-	class VoxelTile : MonoBehaviour, Mapbox.Utils.IObserver<RasterTile>, Mapbox.Utils.IObserver<RawPngRasterTile> {
+	class VoxelTile : MonoBehaviour, Mapbox.Utils.IObserver<RasterTile>, Mapbox.Utils.IObserver<RawPngRasterTile>
+	{
 		[SerializeField]
 		ForwardGeocodeUserInput _geocodeInput;
 
@@ -51,25 +53,31 @@ namespace Mapbox.Examples.Voxels {
 
 		float _tileScale;
 
-		void Awake() {
+		void Awake()
+		{
 			_geocodeInput.OnGeocoderResponse += GeocodeInput_OnGeocoderResponse;
 		}
 
-		void OnDestroy() {
-			if (_geocodeInput) {
+		void OnDestroy()
+		{
+			if (_geocodeInput)
+			{
 				_geocodeInput.OnGeocoderResponse -= GeocodeInput_OnGeocoderResponse;
 			}
 		}
 
-		void Start() {
+		void Start()
+		{
 			_fileSource = MapboxAccess.Instance;
 
 			_raster = new Map<RasterTile>(_fileSource);
 			_elevation = new Map<RawPngRasterTile>(_fileSource);
 
-			if (!string.IsNullOrEmpty(_styleUrl)) {
+			if (!string.IsNullOrEmpty(_styleUrl))
+			{
 				_raster.MapId = _styleUrl;
 			}
+			_elevation.MapId = "mapbox.terrain-rgb";
 
 			_elevation.Subscribe(this);
 			_raster.Subscribe(this);
@@ -78,22 +86,26 @@ namespace Mapbox.Examples.Voxels {
 			FetchWorldData(new Vector2d(-50.98306, -72.96639));
 		}
 
-		void GeocodeInput_OnGeocoderResponse(object sender, EventArgs e) {
+		void GeocodeInput_OnGeocoderResponse(object sender, EventArgs e)
+		{
 			Cleanup();
 			FetchWorldData(_geocodeInput.Coordinate);
 		}
 
-		void Cleanup() {
+		void Cleanup()
+		{
 			StopAllCoroutines();
 			_rasterTexture = null;
 			_elevationTexture = null;
 			_voxels.Clear();
-			foreach (var voxel in _instantiatedVoxels) {
+			foreach (var voxel in _instantiatedVoxels)
+			{
 				Destroy(voxel);
 			}
 		}
 
-		void FetchWorldData(Vector2d coordinates) {
+		void FetchWorldData(Vector2d coordinates)
+		{
 			_tileScale = (_tileWidthInVoxels / 256f) / Conversions.GetTileScaleInMeters((float)coordinates.x, _zoom);
 			var bounds = new Vector2dBounds();
 			bounds.Center = coordinates;
@@ -103,46 +115,57 @@ namespace Mapbox.Examples.Voxels {
 			_elevation.Update();
 		}
 
-		public void OnNext(RasterTile tile) {
-			if (tile.CurrentState == Tile.State.Loaded && !tile.HasError) {
+		public void OnNext(RasterTile tile)
+		{
+			if (tile.CurrentState == Tile.State.Loaded && !tile.HasError)
+			{
 				_rasterTexture = new Texture2D(2, 2);
 				_rasterTexture.LoadImage(tile.Data);
 				TextureScale.Point(_rasterTexture, _tileWidthInVoxels, _tileWidthInVoxels);
 
-				if (ShouldBuildWorld()) {
+				if (ShouldBuildWorld())
+				{
 					BuildVoxelWorld();
 				}
 			}
 		}
 
-		public void OnNext(RawPngRasterTile tile) {
-			if (tile.CurrentState == Tile.State.Loaded && !tile.HasError) {
+		public void OnNext(RawPngRasterTile tile)
+		{
+			if (tile.CurrentState == Tile.State.Loaded && !tile.HasError)
+			{
 				_elevationTexture = new Texture2D(2, 2);
 				_elevationTexture.LoadImage(tile.Data);
 				TextureScale.Point(_elevationTexture, _tileWidthInVoxels, _tileWidthInVoxels);
 
-				if (ShouldBuildWorld()) {
+				if (ShouldBuildWorld())
+				{
 					BuildVoxelWorld();
 				}
 			}
 		}
 
-		bool ShouldBuildWorld() {
+		bool ShouldBuildWorld()
+		{
 			return _rasterTexture != null && _elevationTexture != null;
 		}
 
-		void BuildVoxelWorld() {
+		void BuildVoxelWorld()
+		{
 			var baseHeight = (int)Conversions.GetRelativeHeightFromColor((_elevationTexture.GetPixel(_elevationTexture.width / 2, _elevationTexture.height / 2)),
 																		 _elevationMultiplier * _tileScale);
-			for (int x = 0; x < _rasterTexture.width; x++) {
-				for (int z = 0; z < _rasterTexture.height; z++) {
+			for (int x = 0; x < _rasterTexture.width; x++)
+			{
+				for (int z = 0; z < _rasterTexture.height; z++)
+				{
 					var height = (int)Conversions.GetRelativeHeightFromColor(_elevationTexture.GetPixel(x, z),
 																			 _elevationMultiplier * _tileScale) - baseHeight;
 
 					var startHeight = height - _voxelDepthPadding - 1;
 					var color = _rasterTexture.GetPixel(x, z);
 
-					for (int y = startHeight; y < height; y++) {
+					for (int y = startHeight; y < height; y++)
+					{
 						_voxels.Add(new VoxelData() { Position = new Vector3(x, y, z), Prefab = _voxelFetcher.GetVoxelFromColor(color) });
 					}
 				}
@@ -152,13 +175,17 @@ namespace Mapbox.Examples.Voxels {
 			StartCoroutine(BuildRoutine());
 		}
 
-		IEnumerator BuildRoutine() {
+		IEnumerator BuildRoutine()
+		{
 			var distanceOrderedVoxels = _voxels.OrderBy(x => (_camera.transform.position - x.Position).magnitude).ToList();
 
-			for (int i = 0; i < distanceOrderedVoxels.Count; i += _voxelBatchCount) {
-				for (int j = 0; j < _voxelBatchCount; j++) {
+			for (int i = 0; i < distanceOrderedVoxels.Count; i += _voxelBatchCount)
+			{
+				for (int j = 0; j < _voxelBatchCount; j++)
+				{
 					var index = i + j;
-					if (index < distanceOrderedVoxels.Count) {
+					if (index < distanceOrderedVoxels.Count)
+					{
 						var voxel = distanceOrderedVoxels[index];
 						_instantiatedVoxels.Add(Instantiate(voxel.Prefab, voxel.Position, Quaternion.identity, transform) as GameObject);
 					}
