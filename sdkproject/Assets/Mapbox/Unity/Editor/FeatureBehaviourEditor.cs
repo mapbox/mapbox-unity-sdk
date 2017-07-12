d95712baf0a3ac356b7a2ca800dc2814e4504e00
brnkhy <baran.kahyaoglu@mapbox.com>
2017-07-12 18:09:43 +0300

    add feature behaviour editor script with a button to show feature data, fixes #152 add VertexDebugger to show vertices of a feature

---
+++ sdkproject/Assets/Mapbox/Unity/Editor/FeatureBehaviourEditor.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using Mapbox.Unity.MeshGeneration.Components;
+
+[CustomEditor(typeof(FeatureBehaviour))]
+public class FeatureBehaviourEditor : Editor
+{
+	FeatureBehaviour _beh;
+
+	public void OnEnable()
+	{
+		_beh = (FeatureBehaviour)target;
+	}
+
+	public override void OnInspectorGUI()
+	{
+		DrawDefaultInspector();
+		
+		if (GUILayout.Button("Show Properties"))
+		{
+			_beh.ShowDebugData();
+		}
+	}
+}
