703f0c46c8072ce1ff9ea77fc61e7a25734ce2ac
Baran Kahyaoğlu <baran.kahyaoglu@mapbox.com>
2017-07-13 21:01:24 +0300

    debugging tools and height mod wall option (#155)
    * add feature behaviour editor script with a button to show feature data, fixes #152
    add VertexDebugger to show vertices of a feature
    
    * add draw side walls option to height modifier, fixes #151
    add tooltips to height modifier fields
    
    * Adding sidewalls to building height mods
    
    * Using gizmos for additional customization of drawing.
    This will also be more useful with the new AddMonoBehaviourModifier, since it works on selected.
    
    * Changed to wire cubes.
    
    * rolling back to gameobjects
    
    * Add gizmo version.
    Remove obsolete asset.

---
+++ sdkproject/Assets/Mapbox/Unity/MeshGeneration/Components/VertexDebugger.cs
@@ -0,0 +1,33 @@
+﻿namespace Mapbox.Unity.MeshGeneration.Components
+{
+	using UnityEngine;
+
+	public class VertexDebugger : MonoBehaviour
+	{
+		[Multiline(10)]
+		public string Triangles;
+
+		void Start()
+		{
+			var mf = GetComponent<MeshFilter>();
+			if (mf)
+			{
+				var mesh = mf.mesh;
+				var verts = mesh.vertices;
+				for (int i = 0; i < verts.Length; i++)
+				{
+					var go = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+					go.name = i.ToString();
+					go.transform.SetParent(transform, false);
+					go.transform.localPosition = verts[i];
+				}
+				var tris = mesh.triangles;
+				Triangles = "";
+				for (int i = 0; i < tris.Length; i += 3)
+				{
+					Triangles += tris[i] + "," + tris[i + 1] + "," + tris[i + 2] + "\r\n";
+				}
+			}
+		}
+	}
+}
