944966f4da715fa3bf95c747090de7d065303e77
David Rhodes <david.rhodes@mapbox.com>
2017-07-14 09:53:36 -0600

    Random distribution mod (#159)
    * Initial implementation of "generic" add component modifier
    
    * Round 2: added drag and drop support for monoscript (checks for monobehaviour)
    Organization, naming, and namespaces
    Example script to animate buildings using scriptable object instance (hack)
    
    * Caching Type to avoid the conversion cost every time.
    
    * Added singleton scriptable object
    Changed building animator to use this
    Renamed some assets.
    
    * Organization and proper namespaces for example files.
    
    * Adding example of random distribution inside mesh for styled example.
    Added navmeshtag component.
    
    * Removing navmesh stuff until 5.6+ upgrade (it still works with AddMonoBehaviourModifier, though)
    
    * Oops, actually removed this time.
    
    * Notes and removing debug.
    
    * Organization
    
    * Update submodule

---
+++ sdkproject/Assets/Mapbox/Examples/MeshGenerationBasics/Scripts/AnimateBuildings.cs
@@ -0,0 +1,13 @@
+﻿namespace Mapbox.Examples
+{
+	using UnityEngine;
+
+	public class AnimateBuildings : MonoBehaviour
+	{
+		void Awake()
+		{
+			var runnable = AnimateBuildingsRunnable.Instance;
+			StartCoroutine(runnable.Run(this));
+		}
+	}
+}
