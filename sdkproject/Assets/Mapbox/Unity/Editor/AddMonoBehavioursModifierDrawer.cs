e7a96cb396cde5b4c62dc2dbbc5f58af6a2c4839
David Rhodes <david.rhodes@mapbox.com>
2017-07-14 09:50:48 -0600

    Add component modifier (#158)
    * Initial implementation of "generic" add component modifier
    
    * Round 2: added drag and drop support for monoscript (checks for monobehaviour)
    Organization, naming, and namespaces
    Example script to animate buildings using scriptable object instance (hack)
    
    * Caching Type to avoid the conversion cost every time.
    
    * Added singleton scriptable object
    Changed building animator to use this
    Renamed some assets.
    
    * Organization and proper namespaces for example files.
    
    * add object inspector go modifier
    add highlight feature monobehaviour
    
    * add object selection feature as monobehaviour
    
    * Removing old assets and example scripts (animate buildings)
    Added highlight component as example usage
    Organization and naming.
    
    * Removing MB version.

---
+++ sdkproject/Assets/Mapbox/Unity/Editor/AddMonoBehavioursModifierDrawer.cs
@@ -0,0 +1,56 @@
+﻿namespace Mapbox.Editor
+{
+	using Mapbox.Unity.MeshGeneration.Modifiers;
+	using UnityEditor;
+	using UnityEngine;
+
+	[CustomPropertyDrawer(typeof(AddMonoBehavioursModifierType))]
+	class AddMonoBehavioursModifierDrawer : PropertyDrawer
+	{
+		const int _offset = 40;
+		MonoScript _monoscript;
+
+		public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+		{
+			EditorGUI.BeginProperty(position, label, property);
+
+			position = EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive), label);
+			var scriptRect = new Rect(position.x, position.y, position.width, position.height - _offset);
+			var helpRect = new Rect(position.x, position.y + _offset / 2, position.width, _offset);
+			var typeStringProperty = property.FindPropertyRelative("_typeString");
+			var monoscriptProperty = property.FindPropertyRelative("_script");
+
+			EditorGUI.BeginChangeCheck();
+
+			_monoscript = monoscriptProperty.objectReferenceValue as MonoScript;
+			_monoscript = EditorGUI.ObjectField(scriptRect, _monoscript, typeof(MonoScript), false) as MonoScript;
+
+			if (EditorGUI.EndChangeCheck())
+			{
+				var type = _monoscript.GetClass();
+				if (type != null && type.IsSubclassOf(typeof(MonoBehaviour)))
+				{
+					monoscriptProperty.objectReferenceValue = _monoscript;
+					typeStringProperty.stringValue = _monoscript.GetClass().ToString();
+				}
+				else
+				{
+					monoscriptProperty.objectReferenceValue = null;
+					typeStringProperty.stringValue = "";
+				}
+			}
+
+			if (monoscriptProperty.objectReferenceValue == null)
+			{
+				EditorGUI.HelpBox(helpRect, "Selected object is not a MonoBehaviour!", MessageType.Error);
+			}
+
+			EditorGUI.EndProperty();
+		}
+
+		public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+		{
+			return base.GetPropertyHeight(property, label) + _offset;
+		}
+	}
+}
