7b03c00bdde787eda9ef42e0d3303d3ba11f1268
David Rhodes <david.rhodes@mapbox.com>
2017-07-17 09:41:42 -0600

    Editor turnstile (#147)
    * First-pass at adding turnstile events to unity editor.
    
    * User agent is now correctly formatted.
    Using WWW class to override user-agent for POST.
    
    * Adding turnstile fallback for other platforms.
    
    * Only send turnstile once per day.
    
    * Splitting editor and fallback playerprefs so they are not shared.
    
    * Prefer bundleID over product name

--- sdkproject/Assets/Mapbox/Unity/Constants.cs
+++ sdkproject/Assets/Mapbox/Unity/Constants.cs
@@ -1,28 +1,30 @@
 namespace Mapbox.Unity
 {
-    using UnityEngine;
+	using UnityEngine;
 
-    public static class Constants
-    {
+	public static class Constants
+	{
 		public const string SDK_VERSION = "1.0.0";
-			
-        public static class Path
-        {
+
+		public static class Path
+		{
 			public const string CONFIG_FILE = "MapboxConfiguration.txt";
 			public const string IS_TELEMETRY_ENABLED_KEY = "IS_MAPBOX_TELEMETRY_ENABLED";
+			public const string TELEMETRY_TURNSTILE_LAST_TICKS_EDITOR = "MAPBOX_TELEMETRY_TURNSTILE_LAST_TICKS_EDITOR";
+			public const string TELEMETRY_TURNSTILE_LAST_TICKS_FALLBACK = "MAPBOX_TELEMETRY_TURNSTILE_LAST_TICKS_FALLBACK";
 			public static readonly string MAPBOX_RESOURCES_RELATIVE = System.IO.Path.Combine("Mapbox", "MapboxConfiguration");
 			public static readonly string MAPBOX_RESOURCES_ABSOLUTE = System.IO.Path.Combine(System.IO.Path.Combine(Application.dataPath, "Resources"), "Mapbox");
-        }
+		}
 
-        /// <summary>
-        /// Store common vector constants to avoid the method access cost of Unity's convenience getters.
-        /// </summary>
-        public static class Math
-        {
-            public static readonly Vector3 Vector3Zero = new Vector3(0, 0, 0);
-            public static readonly Vector3 Vector3Up = new Vector3(0, 1, 0);
-            public static readonly Vector3 Vector3Down = new Vector3(0, -1, 0);
+		/// <summary>
+		/// Store common vector constants to avoid the method access cost of Unity's convenience getters.
+		/// </summary>
+		public static class Math
+		{
+			public static readonly Vector3 Vector3Zero = new Vector3(0, 0, 0);
+			public static readonly Vector3 Vector3Up = new Vector3(0, 1, 0);
+			public static readonly Vector3 Vector3Down = new Vector3(0, -1, 0);
 			public static readonly Vector3 Vector3One = new Vector3(1, 1, 1);
 		}
-    }
+	}
 }
