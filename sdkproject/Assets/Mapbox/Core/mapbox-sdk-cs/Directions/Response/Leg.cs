03da79924a40deea42d7c7c1d061d3ace590733a
Wilhelm Berg <BergWerkGIS@users.noreply.github.com>
2017-10-20 23:22:47 +0200

    Mapbox Map Matching API (#302)
    * [wip] map matching
    
    * [wip] MapMatching
    
    * [wip] Map Matching: refactor parameters to enums
    
    * [wip] Map Matching: implement Annotation on Directions.Leg class
    
    * [wip] Map Matching: tests for Annotation
    
    * MapMatching: finish tests
    
    * Lazy instance of Map Matcher added.
    
    * Example script to plot a transform with line renderer.
    
    * Example script to render the results of a map match query.
    
    * Added example of map matching and plotting a raw route for comparison.
    
    * use walking profile
    
    * Serialized profile.
    Log error.
    
    * Adding error for `nosegment`
    
    * Profile and route line thickness
    
    * log request errors, too
    
    * disable transform provider
    
    * fixed description

--- sdkproject/Assets/Mapbox/Core/mapbox-sdk-cs/Directions/Response/Leg.cs
+++ sdkproject/Assets/Mapbox/Core/mapbox-sdk-cs/Directions/Response/Leg.cs
@@ -9,37 +9,50 @@ namespace Mapbox.Directions
 	using System.Collections.Generic;
 	using Mapbox.Json;
 
+
 	/// <summary>
 	/// A Leg from a Directions API call.
 	/// </summary>
 	public class Leg
 	{
+
+
 		/// <summary>
-		/// Gets or sets the steps.
+		/// Depending on the steps parameter, either an Array of RouteStep objects (true, default) or an empty array (false)
 		/// </summary>
 		/// <value>The steps.</value>
 		[JsonProperty("steps")]
 		public List<Step> Steps { get; set; }
 
+
 		/// <summary>
-		/// Gets or sets the summary.
+		/// Depending on the summary parameter, either a String summarizing the route (true, default) or an empty String (false).
 		/// </summary>
 		/// <value>The summary.</value>
 		[JsonProperty("summary")]
 		public string Summary { get; set; }
 
+
 		/// <summary>
-		/// Gets or sets the duration.
+		/// Number indicating the estimated travel time in seconds.
 		/// </summary>
-		/// <value>The duration.</value>
 		[JsonProperty("duration")]
 		public double Duration { get; set; }
 
+
 		/// <summary>
-		/// Gets or sets the distance.
+		/// Number indicating the distance traveled in meters.
 		/// </summary>
-		/// <value>The distance.</value>
 		[JsonProperty("distance")]
 		public double Distance { get; set; }
+
+
+		/// <summary>
+		/// An annotations object that contains additional details about each line segment along the route geometry. Each entry in an annotations field corresponds to a coordinate along the route geometry.
+		/// </summary>
+		[JsonProperty("annotation")]
+		public Annotation Annotation { get; set; }
+
+
 	}
 }
---
+++ sdkproject/Assets/Mapbox/Examples/LocationProvider/Scripts/MapMatchingExample.cs
@@ -0,0 +1,88 @@
+﻿namespace Mapbox.Examples
+{
+	using Mapbox.Unity;
+	using Mapbox.Utils;
+	using Mapbox.Unity.Utilities;
+	using Mapbox.Unity.Map;
+	using System.Collections.Generic;
+	using Mapbox.MapMatching;
+	using UnityEngine;
+
+	public class MapMatchingExample : MonoBehaviour
+	{
+		[SerializeField]
+		AbstractMap _map;
+
+		[SerializeField]
+		LineRenderer _lineRenderer;
+
+		[SerializeField]
+		PlotRoute _originalRoute;
+
+		[SerializeField]
+		Profile _profile;
+
+		MapMatcher _mapMatcher;
+
+		void Awake()
+		{
+			_mapMatcher = MapboxAccess.Instance.MapMatcher;
+		}
+
+		[ContextMenu("Test")]
+		public void Match()
+		{
+			var resource = new MapMatchingResource();
+
+			var coordinates = new List<Vector2d>();
+			foreach (var position in _originalRoute.Positions)
+			{
+				var coord = position.GetGeoPosition(_map.CenterMercator, _map.WorldRelativeScale);
+				coordinates.Add(coord);
+				Debug.Log("MapMatchingExample: " + coord);
+			}
+
+			resource.Coordinates = coordinates.ToArray();
+			resource.Profile = _profile;
+			_mapMatcher.Match(resource, HandleMapMatchResponse);
+		}
+
+		void HandleMapMatchResponse(MapMatching.MapMatchingResponse response)
+		{
+			if (response.HasMatchingError)
+			{
+				Debug.LogError("MapMatchingExample: " + response.MatchingError);
+				return;
+			}
+
+			if (response.HasRequestError)
+			{
+				foreach (var exception in response.RequestExceptions)
+				{
+					Debug.LogError("MapMatchingExample: " + exception);
+				}
+				return;
+			}
+
+			_lineRenderer.positionCount = 0;
+			for (int i = 0, responseTracepointsLength = response.Tracepoints.Length; i < responseTracepointsLength; i++)
+			{
+				var point = response.Tracepoints[i];
+
+				// FIXME: why/how can a point be null?
+				if (point == null)
+				{
+					continue;
+				}
+
+				_lineRenderer.positionCount++;
+				Debug.Log("MapMatchingExample: " + point.Name);
+				Debug.Log("MapMatchingExample: " + point.Location);
+				Debug.Log("MapMatchingExample: " + point.MatchingsIndex);
+				Debug.Log("MapMatchingExample: " + point.WaypointIndex);
+				var position = Conversions.GeoToWorldPosition(point.Location, _map.CenterMercator, _map.WorldRelativeScale).ToVector3xz();
+				_lineRenderer.SetPosition(_lineRenderer.positionCount - 1, position);
+			}
+		}
+	}
+}
