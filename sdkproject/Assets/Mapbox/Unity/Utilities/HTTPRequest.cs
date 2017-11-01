e6294caf5a994b36077fcbd6cc40a40a483a2b7e
David Rhodes <david.rhodes@mapbox.com>
2017-11-01 10:35:20 -0600

    Using built-in timeout and generally optimizing the code.

--- sdkproject/Assets/Mapbox/Unity/Utilities/HTTPRequest.cs
+++ sdkproject/Assets/Mapbox/Unity/Utilities/HTTPRequest.cs
@@ -20,20 +20,17 @@ namespace Mapbox.Unity.Utilities
 	internal sealed class HTTPRequest : IAsyncRequest
 	{
 		private UnityWebRequest _request;
-		private int _timeout;
 		private readonly Action<Response> _callback;
 		bool _wasCancelled;
 
 		public bool IsCompleted { get; private set; }
 
-		// TODO: simplify timeout for Unity 5.6+
-		// https://docs.unity3d.com/ScriptReference/Networking.UnityWebRequest-timeout.html
 		public HTTPRequest(string url, Action<Response> callback, int timeout)
 		{
 			//UnityEngine.Debug.Log("HTTPRequest: " + url);
 			IsCompleted = false;
-			_timeout = timeout;
 			_request = UnityWebRequest.Get(url);
+			_request.timeout = timeout;
 			_callback = callback;
 
 #if UNITY_EDITOR
@@ -57,31 +54,14 @@ namespace Mapbox.Unity.Utilities
 
 		private IEnumerator DoRequest()
 		{
-			_request.Send();
-
-			DateTime timeout = DateTime.Now.AddSeconds(_timeout);
-			bool didTimeout = false;
-
-			while (!_request.isDone)
-			{
-				yield return null;
-				if (DateTime.Now > timeout)
-				{
-					_request.Abort();
-					didTimeout = true;
-					break;
-				}
-			}
+			 yield return _request.Send();
 
 			Response response;
-			if (didTimeout)
+			if (_wasCancelled)
 			{
-				response = Response.FromWebResponse(this, _request, new Exception("Request Timed Out"));
+				Debug.Log("HTTPRequest: " + _wasCancelled);
 			}
-			else
-			{
-				response = Response.FromWebResponse(this, _request, _wasCancelled ? new Exception("Request Cancelled") : null);
-			}
+			response = Response.FromWebResponse(this, _request, _wasCancelled ? new Exception("Request Cancelled") : null);
 
 			_callback(response);
 			_request.Dispose();
