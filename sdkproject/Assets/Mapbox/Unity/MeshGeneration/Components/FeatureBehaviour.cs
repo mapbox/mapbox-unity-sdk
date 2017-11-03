812d2c6392482fae812d45dd7907337576c2c85b
Baran Kahyaoğlu <baran.kahyaoglu@mapbox.com>
2017-11-04 01:09:57 +0300

    add FeatureBehaviourModifier (#337)
    add "show data points" to feature behaviour

--- sdkproject/Assets/Mapbox/Unity/MeshGeneration/Components/FeatureBehaviour.cs
+++ sdkproject/Assets/Mapbox/Unity/MeshGeneration/Components/FeatureBehaviour.cs
@@ -7,12 +7,34 @@ namespace Mapbox.Unity.MeshGeneration.Components
 
 	public class FeatureBehaviour : MonoBehaviour
 	{
-		public Transform Transform { get; set; }
+		public VectorEntity VectorEntity;
+		public Transform Transform;
 		public VectorFeatureUnity Data;
 
 		public void ShowDebugData()
 		{
 			DataString = string.Join("\r\n", Data.Properties.Select(x => x.Key + " - " + x.Value.ToString()).ToArray());
+		}
+
+		public void ShowDataPoints()
+		{
+			foreach (var item in VectorEntity.Feature.Points)
+			{
+				for (int i = 0; i < item.Count; i++)
+				{
+					var go = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+					go.name = i.ToString();
+					go.transform.SetParent(transform, false);
+					go.transform.localPosition = item[i];
+				}
+			}
+		}
+
+		internal void Initialize(VectorEntity ve)
+		{
+			VectorEntity = ve;
+			Transform = transform;
+			Data = ve.Feature;
 		}
 
 		[Multiline(5)]
