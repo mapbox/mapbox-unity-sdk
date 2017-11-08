7464fd2b5fd8e80091f5ab64a4b705a23a1d7e3c
Adarsh Pavani <adarsh.pavani@mapbox.com>
2017-11-07 17:05:28 -0800

    fixed indentation

--- sdkproject/Assets/Mapbox/Unity/Map/RangeTileProvider.cs
+++ sdkproject/Assets/Mapbox/Unity/Map/RangeTileProvider.cs
@@ -18,14 +18,13 @@ namespace Mapbox.Unity.Map
 		{
 			var centerTile = TileCover.CoordinateToTileId(_map.CenterLatitudeLongitude, _map.Zoom);
 			AddTile(new UnwrappedTileId(_map.Zoom, centerTile.X, centerTile.Y));
-
-				for (int x = (int)(centerTile.X - _west); x <= (centerTile.X + _east); x++)
+			for (int x = (int)(centerTile.X - _west); x <= (centerTile.X + _east); x++)
+			{
+				for (int y = (int)(centerTile.Y - _north); y <= (centerTile.Y + _south); y++)
 				{
-					for (int y = (int)(centerTile.Y - _north); y <= (centerTile.Y + _south); y++)
-					{
-						AddTile(new UnwrappedTileId(_map.Zoom, x, y));
-					}
+					AddTile(new UnwrappedTileId(_map.Zoom, x, y));
 				}
+			}
 		}
 	}
 }
