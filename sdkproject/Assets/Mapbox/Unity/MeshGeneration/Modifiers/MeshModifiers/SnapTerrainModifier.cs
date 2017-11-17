e5d2f859fe06a53e024d72e812d3e0c86c34446d
brnkhy <baran.kahyaoglu@mapbox.com>
2017-11-17 03:50:39 +0300

    change snap to terrain raycast modifier to take object position and tile position into account change snap to terrain modifier to take object position into account (due to center change options in mod stack)

--- sdkproject/Assets/Mapbox/Unity/MeshGeneration/Modifiers/MeshModifiers/SnapTerrainModifier.cs
+++ sdkproject/Assets/Mapbox/Unity/MeshGeneration/Modifiers/MeshModifiers/SnapTerrainModifier.cs
@@ -19,7 +19,9 @@
 			{
 				for (int i = 0; i < md.Vertices.Count; i++)
 				{
-					var h = tile.QueryHeightData((float)((md.Vertices[i].x + scaledX / 2) / scaledX), (float)((md.Vertices[i].z + scaledY / 2) / scaledY));
+					var h = tile.QueryHeightData(
+						(float)((md.Vertices[i].x + md.PositionInTile.x + scaledX / 2) / scaledX),
+						(float)((md.Vertices[i].z + md.PositionInTile.z + scaledY / 2) / scaledY));
 					md.Vertices[i] += new Vector3(0, h, 0);
 				}
 			}
@@ -29,7 +31,9 @@
 				{
 					for (int i = 0; i < sub.Count; i++)
 					{
-						var h = tile.QueryHeightData((float)((sub[i].x + scaledX / 2) / scaledX), (float)((sub[i].z + scaledY / 2) / scaledY));
+						var h = tile.QueryHeightData(
+							(float)((sub[i].x + md.PositionInTile.x + scaledX / 2) / scaledX), 
+							(float)((sub[i].z + md.PositionInTile.z + scaledY / 2) / scaledY));
 						sub[i] += new Vector3(0, h, 0);
 					}
 				}
