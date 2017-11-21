b2bd573fddacec1d7d2762b766e11960baaa4db4
David Rhodes <david.rhodes@mapbox.com>
2017-11-21 10:17:51 -0700

    Abstract location provider (#362)
    * First pass abstraction for serializing location providers
    Location struct contains all relevant data now
    simplified location event
    
    * remove orphaned code
    
    * Cleanup.
    
    * Add height offset for map matching route.
    
    * abstracted editor location provider for some additional editor features
    
    * optimizing
    
    * min android version
    
    * Refactor map matching example to use raw location data
    Map matching now passes in other parameters for request (timestamp and radiuses).
    This should improve map matching results, in general.
    
    * Location struct lives in its own file now
    Added update booleans to query if data is fresh.
    
    * Renamed context menu
    
    * Fixing bug where location providers was not working on iOS
    
    * Updating position and rotate components from location provider. Checks if values were updated and use local transforms.
    
    * Spaces converted to tabs
    
    * Formatting.
    
    * Only send Location if heading or position updated.
    
    * Use utils for conversion.
    
    * Fixed missing references.
    
    * pretty print MapMatchingExmaple Debug.Logs
    
    * Device location provider can be used as an editor location provider via Unity Remote app!

---
+++ sdkproject/Assets/Mapbox/Unity/Location/AbstractEditorLocationProvider.cs
@@ -0,0 +1,56 @@
+﻿namespace Mapbox.Unity.Location
+{
+	using System.Collections;
+	using UnityEngine;
+
+	public abstract class AbstractEditorLocationProvider : AbstractLocationProvider
+	{
+		[SerializeField]
+		protected int _accuracy;
+
+		[SerializeField]
+		bool _autoFireEvent;
+
+		[SerializeField]
+		float _updateInterval;
+
+		[SerializeField]
+		bool _sendEvent;
+
+		protected Location _currentLocation;
+
+		WaitForSeconds _wait;
+
+#if UNITY_EDITOR
+		void Awake()
+		{
+			_wait = new WaitForSeconds(_updateInterval);
+			StartCoroutine(QueryLocation());
+		}
+#endif
+
+		IEnumerator QueryLocation()
+		{
+			while (true)
+			{
+				SetLocation();
+				if (_autoFireEvent)
+				{
+					SendLocation(_currentLocation);
+				}
+				yield return _wait;
+			}
+		}
+
+		void OnValidate()
+		{
+			if (_sendEvent)
+			{
+				_sendEvent = false;
+				SendLocation(_currentLocation);
+			}
+		}
+
+		protected abstract void SetLocation();
+	}
+}
