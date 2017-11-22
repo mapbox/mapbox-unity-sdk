3c1cc084afbbec5cf1e44bd28dc7cb91b601b9f7
BergWerkGIS <wb@BergWerk-GIS.at>
2017-11-22 14:02:41 +0100

    simplify duplicate code

--- sdkproject/Assets/Mapbox/Unity/Map/TileErrorHandler.cs
+++ sdkproject/Assets/Mapbox/Unity/Map/TileErrorHandler.cs
@@ -48,32 +48,30 @@
 
 			if (errors.Count > 0)
 			{
-				Debug.LogError(String.Format(
-					"{0} Exception(s) caused on the tile. Tile ID:{1} Tile Type:{4}{2}{3}"
-					, errors.Count
-					, e.TileId
-					, Environment.NewLine
-					, string.Join(Environment.NewLine, errors.Select(ex => ex.Message).ToArray())
-					, e.TileType
-				));
+				Debug.LogError(printMessage(errors, e));
 			}
 
 			if (warnings.Count > 0)
 			{
-				Debug.LogWarning(String.Format(
-					"{0} Exception(s) caused on the tile. Tile ID:{1} Tile Type:{4}{2}{3}"
-					, warnings.Count
-					, e.TileId
-					, Environment.NewLine
-					, string.Join(Environment.NewLine, warnings.Select(ex => ex.Message).ToArray())
-					, e.TileType
-				));
+				Debug.LogWarning(printMessage(warnings, e));
 			}
 
 			if (OnTileError != null)
 			{
 				OnTileError.Invoke(e);
 			}
+		}
+
+		private string printMessage(List<Exception> exceptions, TileErrorEventArgs e)
+		{
+			return string.Format(
+				"{0} Exception(s) caused on the tile. Tile ID:{1} Tile Type:{4}{2}{3}"
+				, exceptions.Count
+				, e.TileId
+				, Environment.NewLine
+				, string.Join(Environment.NewLine, exceptions.Select(ex => ex.Message).ToArray())
+				, e.TileType
+			);
 		}
 
 		void OnDisable()
