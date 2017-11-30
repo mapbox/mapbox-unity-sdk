90487ada8cd1cdd2ef239cd97e979934bf3852b9
BergWerkGIS <wb@BergWerk-GIS.at>
2017-11-30 15:29:46 -0800

    [wip] token api

---
+++ sdkproject/Assets/Mapbox/Core/mapbox-sdk-cs/Tokens/MapboxToken.cs
@@ -0,0 +1,9 @@
+﻿
+
+public class MapboxToken {
+
+
+
+}
+
+
---
+++ sdkproject/Assets/Mapbox/Core/mapbox-sdk-cs/Tokens/MapboxTokenApi.cs
@@ -0,0 +1,35 @@
+﻿
+
+namespace Mapbox.Tokens
+{
+
+	using System.ComponentModel;
+
+
+	public enum MapboxTokenStatus
+	{
+		[Description("")]
+		TokenValid,
+		TokenMalformed,
+		TokenInvalid,
+		TokenExpired,
+		TokenRevoked
+	}
+
+
+	public class MapboxTokenApi
+	{
+
+		public MapboxTokenApi(string access_token)
+		{
+			_accessToken = access_token;
+		}
+
+
+		private string _accessToken;
+
+
+		public void Retrieve() { }
+
+	}
+}
