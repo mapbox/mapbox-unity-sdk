a45f93e236b505cc45dca60b24a3558c3cb3db5f
Abhishek Tripathi <abhishek.tripathi@mapbox.com>
2017-12-14 19:02:17 -0700

    UWP build error fixes.

--- sdkproject/Assets/Mapbox/Core/mapbox-sdk-cs/Tokens/MapboxTokenApi.cs
+++ sdkproject/Assets/Mapbox/Core/mapbox-sdk-cs/Tokens/MapboxTokenApi.cs
@@ -8,6 +8,16 @@ namespace Mapbox.Tokens
 	using System;
 	using System.ComponentModel;
 
+#if PORTABLE || WINDOWS_UWP
+	[AttributeUsage(AttributeTargets.Field, AllowMultiple = false)]
+	public class DescriptionAttribute : Attribute {
+		private readonly string description;
+		public string Description { get { return description; } }
+		public DescriptionAttribute(string description) {
+			this.description = description;
+		}
+	}
+#endif
 
 	public enum MapboxTokenStatus
 	{
