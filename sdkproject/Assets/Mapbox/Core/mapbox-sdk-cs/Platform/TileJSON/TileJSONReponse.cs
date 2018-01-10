2ae13b5c5a93a27c7fb11947f5998e290873d053
BergWerkGIS <wb@BergWerk-GIS.at>
2018-01-10 14:45:53 +0100

    TileJSON objects for API wrapper

---
+++ sdkproject/Assets/Mapbox/Core/mapbox-sdk-cs/Platform/TileJSON/TileJSONReponse.cs
@@ -0,0 +1,80 @@
+﻿namespace Mapbox.Platform.TilesetTileJSON
+{
+
+	using Mapbox.Json;
+
+
+
+	public class TileJSONResponse
+	{
+
+
+		[JsonProperty("attribution")]
+		public string Attribution { get; set; }
+
+
+		[JsonProperty("autoscale")]
+		public bool AutoScale { get; set; }
+
+
+		[JsonProperty("bounds")]
+		public double[] Bounds { get; set; }
+
+
+		[JsonProperty("center")]
+		public double[] Center { get; set; }
+
+
+		[JsonProperty("created")]
+		public long Created { get; set; }
+
+
+		[JsonProperty("description")]
+		public string Description { get; set; }
+
+		[JsonProperty("id")]
+		public long Id { get; set; }
+
+
+		[JsonProperty("maxzoom")]
+		public int MaxZoom { get; set; }
+
+
+		[JsonProperty("minzoom")]
+		public int MinZoom { get; set; }
+
+
+		[JsonProperty("modified")]
+		public long? Modified { get; set; }
+
+
+		[JsonProperty("name")]
+		public string Name { get; set; }
+
+
+		[JsonProperty("private")]
+		public bool Private { get; set; }
+
+
+		[JsonProperty("scheme")]
+		public string Scheme { get; set; }
+
+
+		[JsonProperty("tilejson")]
+		public string TileJSONVersion { get; set; }
+
+
+		[JsonProperty("tiles")]
+		public string[] Tiles { get; set; }
+
+
+		[JsonProperty("vector_layers")]
+		public TileJSONObjectVectorLayer[] VectorLayers { get; set; }
+
+
+		[JsonProperty("webpage")]
+		public string WebPage { get; set; }
+
+
+	}
+}
