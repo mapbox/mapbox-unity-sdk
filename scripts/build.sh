3ec599f8aee966616ad6eb6fc01ecdb01551bc20
Adarshkumar Pavani <adarsh.pavani@mapbox.com>
2018-01-25 22:23:29 -0800

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -22,21 +22,10 @@ echo "Attempting to build $project for OS X"
   -buildOSXUniversalPlayer "$(pwd)/Build/osx/$project.app" 
   -quit
 
-echo "Attempting to build $project for Linux"
-/Applications/Unity/Unity.app/Contents/MacOS/Unity 
-  -batchmode 
-  -nographics 
-  -silent-crashes 
-  -logFile $(pwd)/unity.log 
-  -projectPath $(pwd) 
-  -buildLinuxUniversalPlayer "$(pwd)/Build/linux/$project.exe" 
-  -quit
-
 echo 'Logs from build'
 cat $(pwd)/unity.log
 
 
 echo 'Attempting to zip builds'
-zip -r $(pwd)/Build/linux.zip $(pwd)/Build/linux/
 zip -r $(pwd)/Build/mac.zip $(pwd)/Build/osx/
 zip -r $(pwd)/Build/windows.zip $(pwd)/Build/windows/
