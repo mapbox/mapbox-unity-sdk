f62195b5543fa707840391131bada6d35f2a5e06
Adarsh Pavani <adarsh.pavani@mapbox.com>
2018-02-01 14:18:46 -0500

    adding the right script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,28 +1,30 @@
 #! /bin/sh
 
+set -eu
+
 project="sdkproject"
 
 echo "Attempting to build $project for Windows"
-/Applications/Unity/Unity.app/Contents/MacOS/Unity 
-  -batchmode 
-  -nographics 
-  -silent-crashes 
-  -logFile "$(pwd)/windows-build.log" 
-  -projectPath "$(pwd)/$(project)"
-  -buildWindowsPlayer "$(pwd)/Build/windows/$project.exe"
-  -stackTraceLogType Full
-  -quit 
+/Applications/Unity/Unity.app/Contents/MacOS/Unity \
+-batchmode \
+-nographics \
+-silent-crashes \
+-logFile "$(pwd)/windows-build.log" \
+-projectPath "$(pwd)/${project}" \
+-buildWindowsPlayer "$(pwd)/Build/windows/${project}.exe" \
+-stackTraceLogType Full \
+-quit
 
 echo "Attempting to build $project for OS X"
-/Applications/Unity/Unity.app/Contents/MacOS/Unity 
-  -batchmode 
-  -nographics 
-  -silent-crashes 
-  -logFile "$(pwd)/mac-build.log"
-  -projectPath "$(pwd)/$(project)"
-  -buildOSXUniversalPlayer "$(pwd)/Build/osx/$project.app"
-  -stackTraceLogType Full
-  -quit
+/Applications/Unity/Unity.app/Contents/MacOS/Unity \
+-batchmode \
+-nographics \
+-silent -crashes \
+-logFile "$(pwd)/mac-build.log" \
+-projectPath "$(pwd)/${project}" \
+-buildOSXUniversalPlayer "$(pwd)/Build/osx/${project}.app" \
+-stackTraceLogType Full \
+-quit
 
 echo 'Logs from build'
 cat "$(pwd)/windows-build.log"
