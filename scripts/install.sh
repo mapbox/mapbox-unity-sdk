8cab64d2675fe43a0941a3cf91c07fcdce0de42c
Adarshkumar Pavani <adarsh.pavani@mapbox.com>
2018-02-12 15:39:40 -0800

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -25,7 +25,7 @@ install() {
 
 # See $BASE_URL/$HASH/unity-$VERSION-$PLATFORM.ini for complete list
 # of available packages, where PLATFORM is `osx` or `win`
-cd .. && cd project
+if [ -d "../project" ]; then cd .. && cd project; fi
 if [ -f "Unity-Mac.pkg" ]; then install "Unity-Mac.pkg"; else download "MacEditorInstaller/Unity-$VERSION.pkg" && install "MacEditorInstaller/Unity-$VERSION.pkg"; fi
 wait
 if [ -f "UnitySetup-Windows-Support-for-Editor.pkg" ]; then install "UnitySetup-Windows-Support-for-Editor.pkg"; else download "MacEditorTargetInstaller/UnitySetup-Windows-Support-for-Editor-$VERSION.pkg" && install "MacEditorTargetInstaller/UnitySetup-Windows-Support-for-Editor-$VERSION.pkg"; fi
