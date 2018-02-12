40ecee50432e264ce15f354561e57c375405e3a6
Adarshkumar Pavani <adarsh.pavani@mapbox.com>
2018-02-12 01:09:59 -0800

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -25,9 +25,9 @@ install() {
 
 # See $BASE_URL/$HASH/unity-$VERSION-$PLATFORM.ini for complete list
 # of available packages, where PLATFORM is `osx` or `win`
-
-if [ ! -f "Unity-Mac.pkg" ]; then cd .. && cd project && install "Unity-Mac.pkg"; else download "MacEditorInstaller/Unity-$VERSION.pkg" && install "MacEditorInstaller/Unity-$VERSION.pkg"; fi
+cd .. && cd project
+if [ ! -f "Unity-Mac.pkg" ]; then install "Unity-Mac.pkg"; else download "MacEditorInstaller/Unity-$VERSION.pkg" && install "MacEditorInstaller/Unity-$VERSION.pkg"; fi
 wait
-if [ ! -f "UnitySetup-Windows-Support-for-Editor.pkg" ]; then cd .. && cd project && install "UnitySetup-Windows-Support-for-Editor.pkg"; else download "MacEditorTargetInstaller/UnitySetup-Windows-Support-for-Editor-$VERSION.pkg" && install "MacEditorTargetInstaller/UnitySetup-Windows-Support-for-Editor-$VERSION.pkg"; fi
+if [ ! -f "UnitySetup-Windows-Support-for-Editor.pkg" ]; then install "UnitySetup-Windows-Support-for-Editor.pkg"; else download "MacEditorTargetInstaller/UnitySetup-Windows-Support-for-Editor-$VERSION.pkg" && install "MacEditorTargetInstaller/UnitySetup-Windows-Support-for-Editor-$VERSION.pkg"; fi
 wait
-if [ ! -f "UnitySetup-iOS-Support-for-Editor.pkg" ]; then cd .. && cd project && install "UnitySetup-iOS-Support-for-Editor.pkg"; else download "MacEditorTargetInstaller/UnitySetup-iOS-Support-for-Editor-$VERSION.pkg" && install "MacEditorTargetInstaller/UnitySetup-iOS-Support-for-Editor-$VERSION.pkg"; fi
+if [ ! -f "UnitySetup-iOS-Support-for-Editor.pkg" ]; then install "UnitySetup-iOS-Support-for-Editor.pkg"; else download "MacEditorTargetInstaller/UnitySetup-iOS-Support-for-Editor-$VERSION.pkg" && install "MacEditorTargetInstaller/UnitySetup-iOS-Support-for-Editor-$VERSION.pkg"; fi
