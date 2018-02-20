4bef4a7414012fe68e8d13e65f53cc7821984fea
BergWerkGIS <wb@BergWerk-GIS.at>
2018-02-20 19:10:51 +0100

    Mapbox AR code and examples

---
+++ sdkproject/Assets/MapboxAR/Examples/ARTabletop/Scripts/PlaceMapOnARPlane.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityARInterface;
+
+public class PlaceMapOnARPlane : MonoBehaviour
+{
+
+	[SerializeField]
+	private Transform _mapTransform;
+
+	void Start()
+	{
+		ARPlaneHandler.returnARPlane += PlaceMap;
+		ARPlaneHandler.resetARPlane += ResetPlane;
+	}
+
+	void PlaceMap(BoundedPlane plane)
+	{
+		if (!_mapTransform.gameObject.activeSelf)
+		{
+			_mapTransform.gameObject.SetActive(true);
+		}
+
+		_mapTransform.position = plane.center;
+	}
+
+	void ResetPlane()
+	{
+		_mapTransform.gameObject.SetActive(false);
+	}
+
+	private void OnDisable()
+	{
+		ARPlaneHandler.returnARPlane -= PlaceMap;
+	}
+}
