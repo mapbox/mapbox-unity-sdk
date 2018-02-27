0be7034733922cd558135593b672a4899c822545
brnkhy <baran.kahyaoglu@mapbox.com>
2018-02-27 18:14:14 +0300

    add feature collections and kdtree collection

---
+++ sdkproject/Assets/Mapbox/Unity/MeshGeneration/Data/FeatureCollectionBase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FeatureCollectionBase : ScriptableObject
+{
+	public virtual void Initialize()
+	{
+		
+	}
+
+	public virtual void AddFeature(VectorEntity ve)
+	{
+
+	}
+
+}
---
+++ sdkproject/Assets/Mapbox/Unity/MeshGeneration/Data/KdTreeCollection.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// KdTree Collection is a feature collection using KdTree mainly for distance based searchs like "find all buildings 100m around
+/// player" or "find 10 closest buildings to this point".
+/// KdTree structures focus on search performance so querying for features will be very fast using this collection. On the other
+/// hand it's not good for dynamic/moving entities but we don't have such features on map so it's one of the best options for maps.
+/// </summary>
+
+[CreateAssetMenu(menuName = "Mapbox/Feature Collections/Kd Tree Collection")]
+public class KdTreeCollection : FeatureCollectionBase
+{
+	public KDTree.KDTree<VectorEntity> Entities;
+	public int Count;
+
+	public override void Initialize()
+	{
+		base.Initialize();
+		Entities = new KDTree.KDTree<VectorEntity>(2);
+	}
+
+	public override void AddFeature(VectorEntity ve)
+	{
+		Entities.AddPoint(new double[] { ve.Transform.position.x, ve.Transform.position.z }, ve);
+		Count = Entities.Size;
+	}
+}
