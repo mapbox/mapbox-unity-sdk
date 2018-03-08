a7ee432d217a3bc2f330b83f27ee86bddd4117da
Baran Kahyaoğlu <baran.kahyaoglu@mapbox.com>
2018-03-08 23:12:15 +0300

    Uv mapping (#503)
    * add uv mapping height modifier and a demo scene to show it off
    
    * fix TextureSideWallModifier to respect min_height property
    
    * fix a bug in wall segment calculation in TextureSideWallModifier where CenterSegment feature yielded long first sections
    fix a bug in PolygonModifier where it doesn't create tangents
    fix wrong asset bindings
    
    * fix an issue where TextureSideWallModifier doesn't take atlas section height into consideration
    
    * add a 8 building texture atlas
    
    * add new texture from random google images
    
    * fix a bug where bottomOfMidUv was calculated wrong
    
    * Fix formatting.

---
+++ sdkproject/Assets/Mapbox/Unity/MeshGeneration/Data/AtlasInfo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Mapbox.Unity.MeshGeneration.Data
+{
+	[Serializable]
+	public class AtlasEntity
+	{
+		public Rect TextureRect;
+		public int MidFloorCount;
+		public float ColumnCount;
+
+		public float TopSectionRatio;
+		public float BottomSectionRatio;
+
+		public int PreferredEdgeSectionLength = 10;
+		public float FloorHeight;
+		public float FirstFloorHeight;
+		public float TopFloorHeight;
+	}
+
+	[CreateAssetMenu(menuName = "Mapbox/AtlasInfo")]
+	public class AtlasInfo : ScriptableObject
+	{
+		public List<AtlasEntity> Textures;
+	}
+}
