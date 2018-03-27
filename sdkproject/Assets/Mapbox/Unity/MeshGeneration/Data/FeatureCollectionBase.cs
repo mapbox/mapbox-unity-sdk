ebb818dc1950b71964d053e7fc1e176370e9a325
Abhishek Tripathi <abhishek.tripathi@mapbox.com>
2018-03-27 16:15:06 -0600

    Add namespaces to new files.

--- sdkproject/Assets/Mapbox/Unity/MeshGeneration/Data/FeatureCollectionBase.cs
+++ sdkproject/Assets/Mapbox/Unity/MeshGeneration/Data/FeatureCollectionBase.cs
@@ -1,18 +1,19 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class FeatureCollectionBase : ScriptableObject
+﻿namespace Mapbox.Unity.MeshGeneration
 {
-	public virtual void Initialize()
+	using Mapbox.Unity.MeshGeneration.Data;
+	using UnityEngine;
+
+	public class FeatureCollectionBase : ScriptableObject
 	{
-		
+		public virtual void Initialize()
+		{
+
+		}
+
+		public virtual void AddFeature(VectorEntity ve)
+		{
+
+		}
+
 	}
-
-	public virtual void AddFeature(VectorEntity ve)
-	{
-
-	}
-
 }
--- sdkproject/Assets/Mapbox/Unity/MeshGeneration/Data/KdTreeCollection.cs
+++ sdkproject/Assets/Mapbox/Unity/MeshGeneration/Data/KdTreeCollection.cs
@@ -1,29 +1,32 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-/// <summary>
-/// KdTree Collection is a feature collection using KdTree mainly for distance based searchs like "find all buildings 100m around
-/// player" or "find 10 closest buildings to this point".
-/// KdTree structures focus on search performance so querying for features will be very fast using this collection. On the other
-/// hand it's not good for dynamic/moving entities but we don't have such features on map so it's one of the best options for maps.
-/// </summary>
-
-[CreateAssetMenu(menuName = "Mapbox/Feature Collections/Kd Tree Collection")]
-public class KdTreeCollection : FeatureCollectionBase
+namespace Mapbox.Unity.MeshGeneration
 {
-	public KDTree.KDTree<VectorEntity> Entities;
-	public int Count;
+	using UnityEngine;
+	using KDTree;
+	using Mapbox.Unity.MeshGeneration.Data;
 
-	public override void Initialize()
-	{
-		base.Initialize();
-		Entities = new KDTree.KDTree<VectorEntity>(2);
-	}
+	/// <summary>
+	/// KdTree Collection is a feature collection using KdTree mainly for distance based searchs like "find all buildings 100m around
+	/// player" or "find 10 closest buildings to this point".
+	/// KdTree structures focus on search performance so querying for features will be very fast using this collection. On the other
+	/// hand it's not good for dynamic/moving entities but we don't have such features on map so it's one of the best options for maps.
+	/// </summary>
 
-	public override void AddFeature(VectorEntity ve)
+	[CreateAssetMenu(menuName = "Mapbox/Feature Collections/Kd Tree Collection")]
+	public class KdTreeCollection : FeatureCollectionBase
 	{
-		Entities.AddPoint(new double[] { ve.Transform.position.x, ve.Transform.position.z }, ve);
-		Count = Entities.Size;
+		public KDTree<VectorEntity> Entities;
+		public int Count;
+
+		public override void Initialize()
+		{
+			base.Initialize();
+			Entities = new KDTree.KDTree<VectorEntity>(2);
+		}
+
+		public override void AddFeature(VectorEntity ve)
+		{
+			Entities.AddPoint(new double[] { ve.Transform.position.x, ve.Transform.position.z }, ve);
+			Count = Entities.Size;
+		}
 	}
 }
