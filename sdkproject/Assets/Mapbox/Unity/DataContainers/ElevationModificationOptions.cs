6b148531cf1fe5c6fa46d7b9e7522b4fb783143c
Abhishek Tripathi <abhishek.tripathi@mapbox.com>
2018-04-02 16:56:44 -0600

    Add missing tooltips.

--- sdkproject/Assets/Mapbox/Unity/DataContainers/ElevationModificationOptions.cs
+++ sdkproject/Assets/Mapbox/Unity/DataContainers/ElevationModificationOptions.cs
@@ -1,11 +1,16 @@
 ﻿namespace Mapbox.Unity.Map
 {
 	using System;
+	using UnityEngine;
+
 	[Serializable]
 	public class ElevationModificationOptions
 	{
+		[Tooltip("Number of samples to use for terrain mesh.")]
 		public int sampleCount = 10;
+		[Tooltip("Use world relative scale to scale terrain height.")]
 		public bool useRelativeHeight = false;
+		[Tooltip("Earth radius in Unity units.")]
 		public float earthRadius = 1000f;
 	}
 }
