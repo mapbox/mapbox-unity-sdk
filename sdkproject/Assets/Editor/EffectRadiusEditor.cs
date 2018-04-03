fefb89ce168f11b239f4fd2a3b87fe17dfabb2a7
morgane <morgane.santos@mapbox.com>
2018-04-03 15:53:53 -0700

    Add experimental gizmo and handle scene

---
+++ sdkproject/Assets/Editor/EffectRadiusEditor.cs
@@ -0,0 +1,20 @@
+﻿// Name this script "EffectRadiusEditor"
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(EffectRadius))]
+public class EffectRadiusEditor : Editor
+{
+    public void OnSceneGUI()
+    {
+        EffectRadius t = (target as EffectRadius);
+
+        EditorGUI.BeginChangeCheck();
+        float areaOfEffect = Handles.RadiusHandle(Quaternion.identity, t.transform.position + Vector3.up * 3f, t.areaOfEffect);
+        if (EditorGUI.EndChangeCheck())
+        {
+            Undo.RecordObject(target, "Changed Area Of Effect");
+            t.areaOfEffect = areaOfEffect;
+        }
+    }
+}
---
+++ sdkproject/Assets/EffectRadius.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EffectRadius : MonoBehaviour
+{
+    public float areaOfEffect = 1;
+
+    [SerializeField]
+    public GameObject map;
+
+    void Start() {
+        map = GameObject.Find("Map");
+    }
+    void OnDrawGizmos() {
+		// Gizmos.color = gizmoColor;
+        // Gizmos.DrawWireSphere(transform.position, gizmoRadius);
+		Gizmos.DrawIcon(transform.position + Vector3.up * 3f, "informationIcon.png", true);
+        map = GameObject.Find("Map");
+    }
+}
