dbbfc2bb6c5950483005df41108abbec0f22986b
Abhishek Tripathi <abhishek.tripathi@mapbox.com>
2018-04-09 23:30:27 -0700

    Fix custom mapId being overridden by empty string.

--- sdkproject/Assets/Mapbox/Unity/Editor/PropertyDrawers/VectorLayerPropertiesDrawer.cs
+++ sdkproject/Assets/Mapbox/Unity/Editor/PropertyDrawers/VectorLayerPropertiesDrawer.cs
@@ -54,6 +54,18 @@
 			}
 		}
 
+		string CustomSourceMapId
+		{
+			get
+			{
+				return EditorPrefs.GetString("VectorLayerProperties_customSourceMapId");
+			}
+			set
+			{
+				EditorPrefs.SetString("VectorLayerProperties_customSourceMapId", value);
+			}
+		}
+
 		private GUIContent _mapIdGui = new GUIContent
 		{
 			text = "Map Id",
@@ -108,8 +120,9 @@
 					isActiveProperty.boolValue = true;
 					break;
 				case VectorSourceType.Custom:
-					layerSourceId.stringValue = string.Empty;
+					layerSourceId.stringValue = CustomSourceMapId;
 					EditorGUILayout.PropertyField(sourceOptionsProperty, _mapIdGui);
+					CustomSourceMapId = layerSourceId.stringValue;
 					isActiveProperty.boolValue = true;
 					break;
 				case VectorSourceType.None:
