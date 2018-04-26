3c9312f4cf93135126643638a76c87b22256fee3
jim-martin <jim.martin@mapbox.com>
2018-04-26 10:02:11 -0700

    Correct name and namespace of test script

--- sdkproject/Assets/Mapbox/Examples/6_LocationPrefabs/LocationPrefabsScriptExample.cs
+++ sdkproject/Assets/Mapbox/Examples/6_LocationPrefabs/LocationPrefabsScriptExample.cs
@@ -1,45 +1,49 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿namespace Mapbox.Examples
+{
+	using System.Collections;
+	using System.Collections.Generic;
+	using UnityEngine;
 
-using Mapbox.Unity.Map;
-using Mapbox.Utils;
+	using Mapbox.Unity.Map;
+	using Mapbox.Utils;
 
-public class PrefabPlacementAPITest : MonoBehaviour {
-
-	public AbstractMap map;
-	public GameObject myCustomPrefab;
-
-	// Use this for initialization
-	void Start () 
+	public class LocationPrefabScriptExample : MonoBehaviour
 	{
-		map.OnInitialized += HandleMapInitialized;
-		map.MapVisualizer.OnMapVisualizerStateChanged += HandleMapStateChange;
 
-		//add layers before initialize
-		map.SpawnPrefabAtGeoLocation(myCustomPrefab, new Vector2d(37.784179, -122.401583), HandlePrefabsPlaced);
-		map.Initialize( new Vector2d(37.784179, -122.401583), 16);
-	}
-	
-	// Update is called once per frame
-	void HandleMapInitialized () 
-	{
-		//add layers on initialize
-		
-	}
+		public AbstractMap map;
+		public GameObject myCustomPrefab;
 
-	void HandleMapStateChange ( ModuleState state ) 
-	{
-		if(!(state == ModuleState.Finished))
+		// Use this for initialization
+		void Start()
 		{
-			//add layers on loaded
-			return;
-		}
-	}
+			map.OnInitialized += HandleMapInitialized;
+			map.MapVisualizer.OnMapVisualizerStateChanged += HandleMapStateChange;
 
-	//handle callbacks
-	void HandlePrefabsPlaced( List<GameObject> instances)
-	{
-		Debug.Log(instances.Count);
+			//add layers before initialize
+			map.SpawnPrefabAtGeoLocation(myCustomPrefab, new Vector2d(37.784179, -122.401583), HandlePrefabsPlaced);
+			map.Initialize(new Vector2d(37.784179, -122.401583), 16);
+		}
+
+		// Update is called once per frame
+		void HandleMapInitialized()
+		{
+			//add layers on initialize
+
+		}
+
+		void HandleMapStateChange(ModuleState state)
+		{
+			if (!(state == ModuleState.Finished))
+			{
+				//add layers on loaded
+				return;
+			}
+		}
+
+		//handle callbacks
+		void HandlePrefabsPlaced(List<GameObject> instances)
+		{
+			Debug.Log(instances.Count);
+		}
 	}
 }
