8c05202c3de5b66c76fe74e6f99a65235fbb056d
jim-martin <jim.martin@mapbox.com>
2018-05-01 16:07:25 -0700

    simplify example script

--- sdkproject/Assets/Mapbox/Examples/6_LocationPrefabs/LocationPrefabsScriptExample.cs
+++ sdkproject/Assets/Mapbox/Examples/6_LocationPrefabs/LocationPrefabsScriptExample.cs
@@ -1,4 +1,4 @@
-﻿                                                                                             namespace Mapbox.Examples
+﻿namespace Mapbox.Examples
 {
 	using System.Collections;
 	using System.Collections.Generic;
@@ -9,66 +9,29 @@
 
 	public class LocationPrefabsScriptExample : MonoBehaviour
 	{
-
 		public AbstractMap map;
 
 		//prefab to spawn
-		public GameObject beforeInitialize;
+		public GameObject prefab;
 		//cache of spawned gameobjects
-		private List<GameObject> _beforeInitializeInstances;
-
-		public GameObject afterInitialize;
-		private List<GameObject> _afterInitializeInstances;
-
-		public GameObject afterLoaded;
-		private List<GameObject> _afterLoadedInstances;
-
-
+		private List<GameObject> _prefabInstances;
 
 		// Use this for initialization
 		void Start()
 		{
-			map.OnInitialized += HandleMapInitialized;
-			map.MapVisualizer.OnMapVisualizerStateChanged += HandleMapStateChange;
-
-			//add layers before initialize
-			map.SpawnPrefabByCategory(beforeInitialize, LocationPrefabCategories.ArtsAndEntertainment, 10, HandleBeforeInitializePrefabs, true, "BeforeInitLayer");
+			//add layers before initializing the map
+			map.SpawnPrefabByCategory(prefab, LocationPrefabCategories.ArtsAndEntertainment, 10, HandlePrefabSpawned, true, "SpawnFromScriptLayer");
 			map.Initialize(new Vector2d(37.784179, -122.401583), 16);
 		}
 
-		// Update is called once per frame
-		void HandleMapInitialized()
+		//handle callbacks
+		void HandlePrefabSpawned(List<GameObject> instances)
 		{
-			map.SpawnPrefabByCategory(afterInitialize, LocationPrefabCategories.Shops, 10, HandleAfterInitializePrefabs, true, "AfterInitLayer");
-			map.UpdateMap(map.CenterLatitudeLongitude, map.InitialZoom);
-		}
-
-		void HandleMapStateChange(ModuleState state)
-		{
-			if (!(state == ModuleState.Finished))
+			if(instances.Count > 0)
 			{
-				//add layers on loaded
-				map.SpawnPrefabByCategory(afterLoaded, LocationPrefabCategories.Food, 10, HandleAfterLoadedPrefabs, true, "AfterLoadedLayer");
-				map.UpdateMap(map.CenterLatitudeLongitude, map.InitialZoom);
-				return;
+				Debug.Log(instances[0].name);
 			}
 		}
 
-		//handle callbacks
-		void HandleBeforeInitializePrefabs(List<GameObject> instances)
-		{
-			Debug.Log(instances.Count);
-			//_beforeInitializeInstances.AddRange(instances);
-		}
-
-		void HandleAfterInitializePrefabs(List<GameObject> instances)
-		{
-			//_afterInitializeInstances.AddRange(instances);
-		}
-
-		void HandleAfterLoadedPrefabs(List<GameObject> instances)
-		{
-			//_afterLoadedInstances.AddRange(instances);
-		}
 	}
 }
