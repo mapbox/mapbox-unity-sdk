f940aee25858b5687bd1f84bd7e926b31d3e8f88
Adarsh Pavani <adarsh.pavani@mapbox.com>
2018-05-04 11:07:54 -0700

    ui issue fixed

--- sdkproject/Assets/Mapbox/Unity/Editor/VectorSubLayerTreeView.cs
+++ sdkproject/Assets/Mapbox/Unity/Editor/VectorSubLayerTreeView.cs
@@ -65,11 +65,10 @@
 		{
 			Rect toggleRect = args.rowRect;
 			toggleRect.width = kToggleWidth;
-
 			var item = Layers.GetArrayElementAtIndex(args.item.id);
 			item.FindPropertyRelative("coreOptions.isActive").boolValue = EditorGUI.Toggle(toggleRect, item.FindPropertyRelative("coreOptions.isActive").boolValue);
+			args.rowRect.x += kToggleWidth;
 			args.item.displayName = item.FindPropertyRelative("coreOptions.sublayerName").stringValue;
-
 			base.RowGUI(args);
 
 		}
