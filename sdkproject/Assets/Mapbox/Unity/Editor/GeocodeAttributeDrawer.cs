d8112c9d6fd51aca4ffc7576183e5753d4a6d083
Abhishek Tripathi <abhishek.tripathi@mapbox.com>
2018-05-08 10:37:05 -0700

    Fix geocode attribute drawer not working by itself.

--- sdkproject/Assets/Mapbox/Unity/Editor/GeocodeAttributeDrawer.cs
+++ sdkproject/Assets/Mapbox/Unity/Editor/GeocodeAttributeDrawer.cs
@@ -16,21 +16,17 @@ namespace Mapbox.Editor
 
 		public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
 		{
-			GUILayout.BeginHorizontal();
-			if (string.IsNullOrEmpty(label.text))
-			{
+			float buttonWidth = EditorGUIUtility.singleLineHeight * 4;
 
-				property.stringValue = EditorGUILayout.TextField(property.stringValue);
-			}
-			else
-			{
-				property.stringValue = EditorGUILayout.TextField(label, property.stringValue);
-			}
-			if (GUILayout.Button(new GUIContent(searchButtonContent), (GUIStyle)"minibutton", GUILayout.MaxWidth(100)))
+			Rect fieldRect = new Rect(position.x, position.y, position.width - buttonWidth, EditorGUIUtility.singleLineHeight);
+			Rect buttonRect = new Rect(position.x + position.width - buttonWidth, position.y, buttonWidth, EditorGUIUtility.singleLineHeight);
+
+			EditorGUI.PropertyField(fieldRect, property);
+
+			if (GUI.Button(buttonRect, searchButtonContent))
 			{
 				GeocodeAttributeSearchWindow.Open(property);
 			}
-			GUILayout.EndHorizontal();
 		}
 	}
 }
