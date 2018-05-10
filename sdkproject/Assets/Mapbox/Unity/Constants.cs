e8822c5ac7667b3dd5a50131b17ccb88e79618c2
Abhishek Tripathi <abhishek.tripathi@mapbox.com>
2018-05-10 14:09:26 -0700

    update version number to v1.4.2

--- sdkproject/Assets/Mapbox/Unity/Constants.cs
+++ sdkproject/Assets/Mapbox/Unity/Constants.cs
@@ -4,7 +4,7 @@ namespace Mapbox.Unity
 
 	public static class Constants
 	{
-		public const string SDK_VERSION = "1.4.1";
+		public const string SDK_VERSION = "1.4.2";
 
 		public static class Path
 		{
