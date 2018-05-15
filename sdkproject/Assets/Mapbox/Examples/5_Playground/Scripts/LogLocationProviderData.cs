1c5df5b63acdc8cdb3a987cd40c6ff2a43fb6359
BergWerkGIS <wb@BergWerk-GIS.at>
2018-05-15 05:29:39 -0700

    fix v1.4.2 merge quest using wrong LocationProvider scene and LogLocationProviderData.cs

--- sdkproject/Assets/Mapbox/Examples/5_Playground/Scripts/LogLocationProviderData.cs
+++ sdkproject/Assets/Mapbox/Examples/5_Playground/Scripts/LogLocationProviderData.cs
@@ -6,6 +6,7 @@ namespace Mapbox.Examples.Scripts
 	using System.Collections;
 	using System.Collections.Generic;
 	using System.Globalization;
+	using System.IO;
 	using System.Text;
 	using UnityEngine;
 	using UnityEngine.UI;
@@ -16,25 +17,40 @@ namespace Mapbox.Examples.Scripts
 		[SerializeField]
 		private Text _logText;
 
+		[SerializeField]
+		private Toggle _logToggle;
+
 
 		private CultureInfo _invariantCulture = CultureInfo.InvariantCulture;
+		private bool _logToFile = false;
+		private TextWriter _textWriter = null;
+		/// <summary>column delimiter when logging to file </summary>
+		private string _delimiter = ";";
 
 
-		// Use this for initialization
 		void Start()
 		{
+			Screen.sleepTimeout = SleepTimeout.NeverSleep;
 			LocationProviderFactory.Instance.DefaultLocationProvider.OnLocationUpdated += LocationProvider_OnLocationUpdated;
+
+			if (null != _logToggle)
+			{
+				_logToggle.onValueChanged.AddListener((isOn) => { _logToFile = isOn; });
+			}
 		}
 
 
 		void OnDestroy()
 		{
 			LocationProviderFactory.Instance.DefaultLocationProvider.OnLocationUpdated -= LocationProvider_OnLocationUpdated;
+			closeLogFile();
 		}
 
 
 		void LocationProvider_OnLocationUpdated(Location location)
 		{
+
+			/////////////// GUI logging //////////////////////
 			StringBuilder sb = new StringBuilder();
 
 			sb.AppendLine(string.Format("IsLocationServiceEnabled: {0}", location.IsLocationServiceEnabled));
@@ -53,6 +69,77 @@ namespace Mapbox.Examples.Scripts
 			sb.AppendLine(nullableAsStr<int>(location.SatellitesUsed, "SatellitesUsed:{0} ") + nullableAsStr<int>(location.SatellitesInView, "SatellitesInView:{0}"));
 
 			_logText.text = sb.ToString();
+
+
+			/////////////// file logging //////////////////////
+
+			// start logging to file
+			if (_logToFile && null == _textWriter)
+			{
+				string fileName = "MBX-location-log-" + DateTime.Now.ToString("yyyyMMdd-HHmmss") + ".txt";
+				string persistentPath = Application.persistentDataPath;
+				string fullFilePathAndName = Path.Combine(persistentPath, fileName);
+#if UNITY_EDITOR_WIN || UNITY_STANDALONE_WIN || UNITY_WSA
+				// use `GetFullPath` on that to sanitize the path: replaces `/` returned by `Application.persistentDataPath` with `\`
+				fullFilePathAndName = Path.GetFullPath(fullFilePathAndName);
+#endif
+//#if UNITY_ANDROID
+//				if (RuntimePlatform.Android == Application.platform)
+//				{
+//					// persistentDataPath evaluates to storage/Android/data/<bundle-identifier>/files
+//					// but files writting there cannot be accessed from Windows file explorer
+//					// save into dedicated subdirectory below `files`
+//					string logFolder = Path.Combine(persistentPath, "location-logs");
+//					if (!Directory.Exists(logFolder)) { Directory.CreateDirectory(logFolder); }
+//					fullFilePathAndName = Path.Combine(logFolder, fileName);
+//				}
+//#endif
+
+				Debug.Log("starting new log file: " + fullFilePathAndName);
+
+				_textWriter = new StreamWriter(fullFilePathAndName, false, new UTF8Encoding(false));
+				_textWriter.WriteLine("location service enabled{0}location service initializing{0}location updated{0}heading updated{0}location provider{0}location provider class{0}UTC device{0}UTC location{0}lat{0}lng{0}accuracy[m]{0}user heading[°]{0}device orientation[°]{0}speed{0}has gps fix{0}satellites used{0}satellites in view", _delimiter);
+				_logToggle.GetComponentInChildren<Text>().text = "stop logging";
+			}
+
+
+			// stop logging to file
+			if (!_logToFile && null != _textWriter)
+			{
+				Debug.Log("stop logging to file");
+				_logToggle.GetComponentInChildren<Text>().text = "start logging";
+				closeLogFile();
+			}
+
+
+			// write line to log file
+			if (_logToFile && null != _textWriter)
+			{
+				string[] lineTokens = new string[]
+				{
+					location.IsLocationServiceEnabled.ToString(),
+					location.IsLocationServiceInitializing.ToString(),
+					location.IsLocationUpdated.ToString(),
+					location.IsUserHeadingUpdated.ToString(),
+					location.Provider,
+					LocationProviderFactory.Instance.DefaultLocationProvider.GetType().Name,
+					DateTime.UtcNow.ToString("yyyyMMdd-HHmmss.fff"),
+					UnixTimestampUtils.From(location.Timestamp).ToString("yyyyMMdd-HHmmss.fff"),
+					string.Format(_invariantCulture, "{0:0.00000000}", location.LatitudeLongitude.x),
+					string.Format(_invariantCulture, "{0:0.00000000}", location.LatitudeLongitude.y),
+					string.Format(_invariantCulture, "{0:0.0}", location.Accuracy),
+					string.Format(_invariantCulture, "{0:0.0}", location.UserHeading),
+					string.Format(_invariantCulture, "{0:0.0}", location.DeviceOrientation),
+					nullableAsStr<float>(location.SpeedKmPerHour, "{0:0.0}"),
+					nullableAsStr<bool>(location.HasGpsFix, "{0}"),
+					nullableAsStr<int>(location.SatellitesUsed, "{0}"),
+					nullableAsStr<int>(location.SatellitesInView, "{0}")
+				};
+
+				string logMsg = string.Join(_delimiter, lineTokens);
+				Debug.Log(logMsg);
+				_textWriter.WriteLine(logMsg);
+			}
 		}
 
 
@@ -65,11 +152,18 @@ namespace Mapbox.Examples.Scripts
 		}
 
 
-		// Update is called once per frame
-		void Update()
+		private void closeLogFile()
 		{
-
+			if (null == _textWriter) { return; }
+			Debug.Log("closing stream writer");
+			_textWriter.Flush();
+			_textWriter.Close();
+			_textWriter.Dispose();
+			_textWriter = null;
 		}
+
+
+		void Update() { }
 
 
 
