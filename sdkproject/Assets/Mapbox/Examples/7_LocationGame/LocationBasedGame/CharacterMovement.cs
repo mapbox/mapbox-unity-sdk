f15635ba4f59931a9e70293f60b1fb2c69a760ad
brnkhy <baran.kahyaoglu@mapbox.com>
2018-05-18 19:24:06 +0300

    add height mapping for traffic/cars add player character movement and animations

--- sdkproject/Assets/Mapbox/Examples/7_LocationGame/LocationBasedGame/CharacterMovement.cs
+++ sdkproject/Assets/Mapbox/Examples/7_LocationGame/LocationBasedGame/CharacterMovement.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class CharacterMovement : MonoBehaviour
 {
 	public Material[] Materials;
+	public Transform Target;
+	public Animator CharacterAnimator;
+	public float Speed;
 
 	void Start()
 	{
@@ -16,6 +19,17 @@ public class CharacterMovement : MonoBehaviour
 		foreach (var item in Materials)
 		{
 			item.SetVector("_CharacterPosition", transform.position);
+		}
+
+		if (Vector3.Distance(transform.position, Target.position) > 0.1f)
+		{
+			transform.LookAt(Target.position);
+			transform.Translate(Vector3.forward * Speed);
+			CharacterAnimator.SetBool("IsWalking", true);
+		}
+		else
+		{
+			CharacterAnimator.SetBool("IsWalking", false);
 		}
 	}
 }
