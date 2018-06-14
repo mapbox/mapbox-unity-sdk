e1b4c34df98d19185357453c6730e1102c29e74b
brnkhy <baran.kahyaoglu@mapbox.com>
2018-06-15 01:17:31 +0300

    add namespace to demo scene script files move script files to demo scripts folder

---
+++ sdkproject/Assets/Mapbox/Examples/Scripts/TrafficUvAnimator.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Mapbox.Examples
+{
+	public class TrafficUvAnimator : MonoBehaviour
+	{
+		public Material[] Materials;
+		public float Speed;
+		private Vector2 _offset;
+
+		void Start()
+		{
+
+		}
+
+		void Update()
+		{
+			_offset.Set(_offset.x + Time.deltaTime * Speed, 0.2f);
+
+			foreach (var item in Materials)
+			{
+				item.SetTextureOffset("_MainTex", _offset);
+			}
+		}
+	}
+}
+
