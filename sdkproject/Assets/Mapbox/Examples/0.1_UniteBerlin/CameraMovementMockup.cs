194fc5df99203ef4117ac111a69251a6930c7051
Abhishek Tripathi <abhishek.tripathi@mapbox.com>
2018-06-15 13:43:43 -0700

    Unite Berlin demo work.

---
+++ sdkproject/Assets/Mapbox/Examples/0.1_UniteBerlin/CameraMovementMockup.cs
@@ -0,0 +1,26 @@
+﻿namespace Mapbox.Examples
+{
+	using System.Collections;
+	using System.Collections.Generic;
+	using UnityEngine;
+
+	public class CameraMovementMockup : MonoBehaviour
+	{
+		public Transform Target;
+		public float Speed;
+
+		void Start()
+		{
+
+		}
+
+		void Update()
+		{
+			if (Vector3.Distance(transform.position, Target.position) > 0.1f)
+			{
+				transform.LookAt(Target.position);
+				transform.Translate(Vector3.forward * Speed);
+			}
+		}
+	}
+}
---
+++ sdkproject/Assets/Mapbox/Examples/0.1_UniteBerlin/PlaceOnARPlane.cs
@@ -0,0 +1,25 @@
+﻿namespace Mapbox.Unity.Ar
+{
+
+	using System.Collections;
+	using System.Collections.Generic;
+	using UnityEngine;
+	using UnityARInterface;
+
+	public class PlaceOnARPlane : MonoBehaviour
+	{
+
+		void Start()
+		{
+			ARInterface.planeAdded += PlaceOnPlane;
+			ARInterface.planeUpdated += PlaceOnPlane;
+		}
+
+		void PlaceOnPlane(BoundedPlane plane)
+		{
+			var pos = transform.position;
+			transform.position = new Vector3(pos.x, plane.center.y, pos.z);
+		}
+
+	}
+}
