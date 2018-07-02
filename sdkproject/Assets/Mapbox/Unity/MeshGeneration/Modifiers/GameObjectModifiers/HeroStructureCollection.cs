6f4d081bcca69cb1d0843de705037ad05c5476a8
greglemonmapbox <greg.lemon@mapbox.com>
2018-07-02 11:28:57 -0700

    Initial dev for hero structure spawning

---
+++ sdkproject/Assets/Mapbox/Unity/MeshGeneration/Modifiers/GameObjectModifiers/HeroStructureCollection.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using KDTree;
+using Mapbox.Unity.MeshGeneration;
+
+[CreateAssetMenu(menuName = "Mapbox/HeroStructureCollection")]
+public class HeroStructureCollection : ScriptableObject {
+
+	public List<HeroStructureDataBundle> heroStructures = new List<HeroStructureDataBundle>();
+	public KdTreeCollection Collection;
+
+	private void BuildKDTree()
+	{
+		
+	}
+
+	private void OnValidate()
+	{
+		Debug.Log("OnValidate");
+		BuildKDTree();
+	}
+}
---
+++ sdkproject/Assets/Mapbox/Unity/MeshGeneration/Modifiers/GameObjectModifiers/HeroStructureDataBundle.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using Mapbox.Editor;
+
+using Mapbox.Unity.Map;
+using Mapbox.Utils;
+using Mapbox.Unity.Utilities;
+using Mapbox.VectorTile.Geometry;
+using Mapbox.Unity.MeshGeneration.Interfaces;
+
+[System.Serializable]
+public class HeroStructureDataBundle
+{
+	public bool active = true;
+	public GameObject prefab;
+	[Geocode]
+	public string latLon;
+	public string prefabPath;
+}
