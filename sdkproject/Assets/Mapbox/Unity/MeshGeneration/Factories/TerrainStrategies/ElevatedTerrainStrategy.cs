3b708231dd1a141eb50cd4559810cbfeac98ed4b
brnkhy <baran.kahyaoglu@mapbox.com>
2018-09-05 22:40:28 +0300

    change ElevatedTerrainStrategy and ElevatedTerrainWithSidesStrategy classes to create flat terrain mesh on height data errors

--- sdkproject/Assets/Mapbox/Unity/MeshGeneration/Factories/TerrainStrategies/ElevatedTerrainStrategy.cs
+++ sdkproject/Assets/Mapbox/Unity/MeshGeneration/Factories/TerrainStrategies/ElevatedTerrainStrategy.cs
@@ -59,7 +59,7 @@ namespace Mapbox.Unity.MeshGeneration.Factories.TerrainStrategies
 
 		public override void DataErrorOccurred(UnityTile t, TileErrorEventArgs e)
 		{
-
+			ResetToFlatMesh(t);
 		}
 
 		public override void PostProcessTile(UnityTile tile)
@@ -192,23 +192,30 @@ namespace Mapbox.Unity.MeshGeneration.Factories.TerrainStrategies
 
 		private void ResetToFlatMesh(UnityTile tile)
 		{
-			tile.MeshFilter.mesh.GetVertices(_currentTileMeshData.Vertices);
-			tile.MeshFilter.mesh.GetNormals(_currentTileMeshData.Normals);
-
-			_counter = _currentTileMeshData.Vertices.Count;
-			for (int i = 0; i < _counter; i++)
+			if (tile.MeshFilter.mesh.vertexCount == 0)
 			{
-				_currentTileMeshData.Vertices[i] = new Vector3(
-					_currentTileMeshData.Vertices[i].x,
-					0,
-					_currentTileMeshData.Vertices[i].z);
-				_currentTileMeshData.Normals[i] = Mapbox.Unity.Constants.Math.Vector3Up;
+				CreateBaseMesh(tile);
 			}
+			else
+			{
+				tile.MeshFilter.mesh.GetVertices(_currentTileMeshData.Vertices);
+				tile.MeshFilter.mesh.GetNormals(_currentTileMeshData.Normals);
 
-			tile.MeshFilter.mesh.SetVertices(_currentTileMeshData.Vertices);
-			tile.MeshFilter.mesh.SetNormals(_currentTileMeshData.Normals);
+				_counter = _currentTileMeshData.Vertices.Count;
+				for (int i = 0; i < _counter; i++)
+				{
+					_currentTileMeshData.Vertices[i] = new Vector3(
+						_currentTileMeshData.Vertices[i].x,
+						0,
+						_currentTileMeshData.Vertices[i].z);
+					_currentTileMeshData.Normals[i] = Mapbox.Unity.Constants.Math.Vector3Up;
+				}
 
-			tile.MeshFilter.mesh.RecalculateBounds();
+				tile.MeshFilter.mesh.SetVertices(_currentTileMeshData.Vertices);
+				tile.MeshFilter.mesh.SetNormals(_currentTileMeshData.Normals);
+
+				tile.MeshFilter.mesh.RecalculateBounds();
+			}
 		}
 
 		/// <summary>
