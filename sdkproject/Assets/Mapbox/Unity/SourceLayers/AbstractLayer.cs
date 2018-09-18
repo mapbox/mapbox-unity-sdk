f1c075cf1a5efcb2dcce1685b711b0c07c71ac04
Abhishek Tripathi <abhishek.tripathi@mapbox.com>
2018-09-17 21:09:52 -0700

    Add VectorLayerUpdateArgs + overload for NotifyUpdateLayer

--- sdkproject/Assets/Mapbox/Unity/SourceLayers/AbstractLayer.cs
+++ sdkproject/Assets/Mapbox/Unity/SourceLayers/AbstractLayer.cs
@@ -1,6 +1,7 @@
 ﻿namespace Mapbox.Unity.Map
 {
 	using Mapbox.Unity.MeshGeneration.Factories;
+	using Mapbox.Unity.MeshGeneration.Interfaces;
 
 	public class LayerUpdateArgs : System.EventArgs
 	{
@@ -9,9 +10,22 @@
 		public bool effectsVectorLayer;
 	}
 
+	public class VectorLayerUpdateArgs : LayerUpdateArgs
+	{
+		public LayerVisualizerBase visualizer;
+	}
+
 	public class AbstractLayer
 	{
 		public event System.EventHandler UpdateLayer;
+		protected virtual void NotifyUpdateLayer(LayerUpdateArgs layerUpdateArgs)
+		{
+			System.EventHandler handler = UpdateLayer;
+			if (handler != null)
+			{
+				handler(this, layerUpdateArgs);
+			}
+		}
 		protected virtual void NotifyUpdateLayer(AbstractTileFactory factory, MapboxDataProperty prop, bool effectsVectorLayer = false)
 		{
 			System.EventHandler handler = UpdateLayer;
