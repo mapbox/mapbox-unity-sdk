55a1d2001427e93e5b19695274d438b60e5318df
brnkhy <baran.kahyaoglu@mapbox.com>
2018-09-25 03:16:25 +0300

    add api methods to terrain and imagery layers

--- sdkproject/Assets/Mapbox/Unity/DataContainers/UnityLayerOptions.cs
+++ sdkproject/Assets/Mapbox/Unity/DataContainers/UnityLayerOptions.cs
@@ -1,14 +1,21 @@
-﻿namespace Mapbox.Unity.Map
+﻿using Mapbox.Unity.MeshGeneration.Data;
+
+namespace Mapbox.Unity.Map
 {
 	using System;
 	using UnityEngine;
 
 	[Serializable]
-	public class UnityLayerOptions
+	public class UnityLayerOptions : MapboxDataProperty
 	{
 		[Tooltip("Add terrain tiles to Unity Layer")]
 		public bool addToLayer = false;
 		[Tooltip("Unity Layer id to which terrain tiles will get added.")]
 		public int layerId = 0;
+
+		public override void UpdateProperty(UnityTile tile)
+		{
+			tile.gameObject.layer = layerId;
+		}
 	}
 }
