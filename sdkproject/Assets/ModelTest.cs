ada0a161e399eac46b2d93c34ef6a01157c591c8
brnkhy <baran.kahyaoglu@mapbox.com>
2018-09-28 22:47:11 +0300

    add api methods for feature mesh generations add ModelTest.cs file to test mesh generation api methods

---
+++ sdkproject/Assets/ModelTest.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using Mapbox.Unity.Map;
+using Mapbox.Unity.MeshGeneration.Interfaces;
+using UnityEngine;
+
+public class ModelTest : MonoBehaviour
+{
+	private AbstractMap _abstractMap;
+
+	void Start()
+	{
+		_abstractMap = FindObjectOfType<AbstractMap>();
+	}
+
+	[ContextMenu("Disable Extrusion")]
+	public void DisableExtrusion()
+	{
+		foreach (var layer in _abstractMap.VectorData.GetAllFeatureLayers())
+		{
+			layer.DisableExtrusion();
+		}
+	}
+
+	[ContextMenu("Set Absolute Extrusion")]
+	public void SetAbsoluteExtrusion()
+	{
+		foreach (var layer in _abstractMap.VectorData.GetAllFeatureLayers())
+		{
+			layer.EnableAbsoluteExtrusion(ExtrusionGeometryType.RoofAndSide, 10, 1);
+		}
+	}
+
+	[ContextMenu("Set Property Extrusion")]
+	public void SetPropertyExtrusion()
+	{
+		foreach (var layer in _abstractMap.VectorData.GetAllFeatureLayers())
+		{
+			layer.EnablePropertyExtrusion(ExtrusionGeometryType.RoofAndSide);
+		}
+	}
+
+	[ContextMenu("Set Minimum Height Extrusion")]
+	public void SetMinimumExtrusion()
+	{
+		foreach (var layer in _abstractMap.VectorData.GetAllFeatureLayers())
+		{
+			layer.EnableMinExtrusion(ExtrusionGeometryType.RoofAndSide);
+		}
+	}
+
+	[ContextMenu("Set Maximum Height Extrusion")]
+	public void SetMaximumExtrusion()
+	{
+		foreach (var layer in _abstractMap.VectorData.GetAllFeatureLayers())
+		{
+			layer.EnableMaxExtrusion(ExtrusionGeometryType.RoofAndSide);
+		}
+	}
+
+	[ContextMenu("Set Range Extrusion")]
+	public void SetRangeExtrusion()
+	{
+		foreach (var layer in _abstractMap.VectorData.GetAllFeatureLayers())
+		{
+			layer.EnableRangeExtrusion(ExtrusionGeometryType.RoofAndSide, 10, 20);
+		}
+	}
+
+	[ContextMenu("Set Absolute Height to 35")]
+	public void SetAbsoluteHeight()
+	{
+		foreach (var layer in _abstractMap.VectorData.GetAllFeatureLayers())
+		{
+			layer.SetAbsoluteHeight(35);
+		}
+	}
+
+	[ContextMenu("Set Height Range to 35-70")]
+	public void SetHeightRange()
+	{
+		foreach (var layer in _abstractMap.VectorData.GetAllFeatureLayers())
+		{
+			layer.SetHeightRange(35, 70);
+		}
+	}
+
+	[ContextMenu("Set Extrusion Multiplier to x2")]
+	public void SetExtrusionMultiplier()
+	{
+		foreach (var layer in _abstractMap.VectorData.GetAllFeatureLayers())
+		{
+			layer.SetExtrusionMultiplier(2);
+		}
+	}
+
+	[ContextMenu("Enable terrain snapping")]
+	public void EnableTerrainSnapping()
+	{
+		foreach (var layer in _abstractMap.VectorData.GetAllFeatureLayers())
+		{
+			layer.EnableSnapingTerrain(!layer.coreOptions.snapToTerrain);
+		}
+	}
+
+	[ContextMenu("Enable mesh combining")]
+	public void EnableMeshCombining()
+	{
+		foreach (var layer in _abstractMap.VectorData.GetAllFeatureLayers())
+		{
+			layer.EnableCombiningMeshes(true);
+		}
+	}
+
+	[ContextMenu("Set Feature Collider")]
+	public void SetFeatureCollider()
+	{
+		foreach (var layer in _abstractMap.VectorData.GetAllFeatureLayers())
+		{
+			layer.SetFeatureCollider((ColliderType)UnityEngine.Random.RandomRange(0,4));
+		}
+	}
+
+	[ContextMenu("Set Primitive Type")]
+	public void SetPrimitiveType()
+	{
+		foreach (var layer in _abstractMap.VectorData.GetAllFeatureLayers())
+		{
+			layer.SetPrimitiveType((VectorPrimitiveType)UnityEngine.Random.RandomRange(0,2));
+		}
+	}
+}
