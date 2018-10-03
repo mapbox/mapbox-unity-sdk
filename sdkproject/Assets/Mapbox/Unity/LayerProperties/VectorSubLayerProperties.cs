d5a8d22a809ee93732b967490992fb5a01e75911
Abhishek Tripathi <abhishek.tripathi@mapbox.com>
2018-10-02 22:45:49 -0700

    Add missing method.

--- sdkproject/Assets/Mapbox/Unity/LayerProperties/VectorSubLayerProperties.cs
+++ sdkproject/Assets/Mapbox/Unity/LayerProperties/VectorSubLayerProperties.cs
@@ -144,6 +144,28 @@ namespace Mapbox.Unity.Map
 			coreOptions.isActive = active;
 			coreOptions.HasChanged = true;
 		}
+
+		/// <summary>
+		/// Switch layer to custom style using provided mesh and game object modifier
+		/// </summary>
+		/// <param name="meshModifiers">Mesh modifiers to be used in layer</param>
+		/// <param name="gameObjectModifiers">Game object modifiers to be used in layer</param>
+		public virtual void CreateCustomStyle(List<MeshModifier> meshModifiers, List<GameObjectModifier> gameObjectModifiers)
+		{
+			coreOptions.geometryType = VectorPrimitiveType.Custom;
+			coreOptions.HasChanged = true;
+
+			MeshModifiers.Clear();
+			foreach (var meshModifier in meshModifiers)
+			{
+				MeshModifiers.Add(meshModifier);
+			}
+			foreach (var goModifier in gameObjectModifiers)
+			{
+				GoModifiers.Add(goModifier);
+			}
+			HasChanged = true;
+		}
 		#endregion
 	}
 }
