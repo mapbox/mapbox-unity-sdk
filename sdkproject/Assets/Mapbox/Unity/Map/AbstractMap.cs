a5946fa4bde7433a8a516b6463d8d22b6f6bfce1
brnkhy <baran.kahyaoglu@mapbox.com>
2018-10-11 01:42:39 +0300

    revert AbstractMap change from rpevious commit

--- sdkproject/Assets/Mapbox/Unity/Map/AbstractMap.cs
+++ sdkproject/Assets/Mapbox/Unity/Map/AbstractMap.cs
@@ -562,7 +562,7 @@ namespace Mapbox.Unity.Map
 			_options.extentOptions.defaultExtents.PropertyHasChanged += (object sender, System.EventArgs eventArgs) =>
 			{
 				//take care of redraw map business...
-				OnTileProviderChanged();
+				_tileProvider.UpdateTileExtent();
 			};
 
 			_options.placementOptions.PropertyHasChanged += (object sender, System.EventArgs eventArgs) =>
