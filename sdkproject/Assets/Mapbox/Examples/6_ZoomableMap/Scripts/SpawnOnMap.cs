50c474fd556f78b50923cc28c6768a380d5875aa
greglemonmapbox <greg.lemon@mapbox.com>
2018-10-17 14:01:00 -0700

    Changed SpawnOnMap to wait for map to pass ModuleState.Finished before spawning prefabs.

--- sdkproject/Assets/Mapbox/Examples/6_ZoomableMap/Scripts/SpawnOnMap.cs
+++ sdkproject/Assets/Mapbox/Examples/6_ZoomableMap/Scripts/SpawnOnMap.cs
@@ -5,6 +5,7 @@
 	using Mapbox.Unity.Map;
 	using Mapbox.Unity.MeshGeneration.Factories;
 	using Mapbox.Unity.Utilities;
+	using System.Collections;
 	using System.Collections.Generic;
 
 	public class SpawnOnMap : MonoBehaviour
@@ -25,10 +26,42 @@
 
 		List<GameObject> _spawnedObjects;
 
+		private bool _hasSpawned;
+
 		void Start()
 		{
 			_locations = new Vector2d[_locationStrings.Length];
 			_spawnedObjects = new List<GameObject>();
+			_map.MapVisualizer.OnMapVisualizerStateChanged += OnMapReady;
+		}
+
+		private void Update()
+		{
+			if(!_hasSpawned)
+			{
+				return;
+			}
+			int count = _spawnedObjects.Count;
+			for (int i = 0; i < count; i++)
+			{
+				var spawnedObject = _spawnedObjects[i];
+				var location = _locations[i];
+				spawnedObject.transform.localPosition = _map.GeoToWorldPosition(location, true);
+				spawnedObject.transform.localScale = new Vector3(_spawnScale, _spawnScale, _spawnScale);
+			}
+		}
+
+		private void OnMapReady(ModuleState moduleState)
+		{
+			if (moduleState == ModuleState.Finished)
+			{
+				_map.MapVisualizer.OnMapVisualizerStateChanged -= OnMapReady;
+				StartCoroutine(SpawnPrefab());
+			}
+		}
+
+		private IEnumerator SpawnPrefab()
+		{
 			for (int i = 0; i < _locationStrings.Length; i++)
 			{
 				var locationString = _locationStrings[i];
@@ -38,18 +71,8 @@
 				instance.transform.localScale = new Vector3(_spawnScale, _spawnScale, _spawnScale);
 				_spawnedObjects.Add(instance);
 			}
-		}
-
-		private void Update()
-		{
-			int count = _spawnedObjects.Count;
-			for (int i = 0; i < count; i++)
-			{
-				var spawnedObject = _spawnedObjects[i];
-				var location = _locations[i];
-				spawnedObject.transform.localPosition = _map.GeoToWorldPosition(location, true);
-				spawnedObject.transform.localScale = new Vector3(_spawnScale, _spawnScale, _spawnScale);
-			}
+			_hasSpawned = true;
+			yield return null;
 		}
 	}
 }
