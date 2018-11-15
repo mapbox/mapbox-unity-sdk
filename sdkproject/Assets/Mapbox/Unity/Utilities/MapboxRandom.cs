915a9f79b296b879b7b3dea9f9a3b809c1b437a0
greglemonmapbox <greg.lemon@mapbox.com>
2018-11-15 11:55:57 -0800

    Added comments to MapboxRandom methods.

--- sdkproject/Assets/Mapbox/Unity/Utilities/MapboxRandom.cs
+++ sdkproject/Assets/Mapbox/Unity/Utilities/MapboxRandom.cs
@@ -7,11 +7,22 @@
 
 		Random random = new Random();
 
+		/// <summary>
+		/// Init the specified seed.
+		/// </summary>
+		/// <param name="seed">Seed.</param>
 		public void Init(int seed)
 		{
 			random = new Random(seed);
 		}
 
+		/// <summary>
+		/// Get a random int value between min and max; optionally provide a seed for randomization.
+		/// </summary>
+		/// <returns>The range.</returns>
+		/// <param name="min">Minimum.</param>
+		/// <param name="max">Max.</param>
+		/// <param name="seed">Seed.</param>
 		public int Range(int min, int max, int? seed = null)
 		{
 			if (seed != null)
@@ -21,6 +32,13 @@
 			return random.Next(min, max);
 		}
 
+		/// <summary>
+		/// Get a random float value between min and max; optionally provide a seed for randomization.
+		/// </summary>
+		/// <returns>The range.</returns>
+		/// <param name="min">Minimum.</param>
+		/// <param name="max">Max.</param>
+		/// <param name="seed">Seed.</param>
 		public float Range(float min, float max, int? seed = null)
 		{
 			if (seed != null)
