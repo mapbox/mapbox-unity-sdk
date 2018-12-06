fc642a8844986d751bc91e6dd3e5e4df8918256e
Abhishek Tripathi <abhishek.tripathi@mapbox.com>
2018-12-05 17:15:24 -0800

    Adding new script for AR Foundation

---
+++ sdkproject/Assets/MapboxAR/Examples/ARTabletop/Scripts/PlaceOnPlane.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.XR;
+using UnityEngine.XR.ARFoundation;
+
+[RequireComponent(typeof(ARSessionOrigin))]
+public class PlaceOnPlane : MonoBehaviour
+{
+	[SerializeField]
+	[Tooltip("Instantiates this prefab on a plane at the touch location.")]
+	GameObject m_PlacedPrefab;
+
+	/// <summary>
+	/// The prefab to instantiate on touch.
+	/// </summary>
+	public GameObject placedPrefab
+	{
+		get { return m_PlacedPrefab; }
+		set { m_PlacedPrefab = value; }
+	}
+
+	/// <summary>
+	/// The object instantiated as a result of a successful raycast intersection with a plane.
+	/// </summary>
+	public GameObject spawnedObject { get; private set; }
+
+	ARSessionOrigin m_SessionOrigin;
+
+	static List<ARRaycastHit> s_Hits = new List<ARRaycastHit>();
+
+	void Awake()
+	{
+		m_SessionOrigin = GetComponent<ARSessionOrigin>();
+	}
+
+	void Update()
+	{
+		if (Input.touchCount > 0)
+		{
+			Touch touch = Input.GetTouch(0);
+
+			if (m_SessionOrigin.Raycast(touch.position, s_Hits, TrackableType.PlaneWithinPolygon))
+			{
+				Pose hitPose = s_Hits[0].pose;
+
+				if (spawnedObject == null)
+				{
+					spawnedObject = Instantiate(m_PlacedPrefab, hitPose.position, hitPose.rotation);
+				}
+				else
+				{
+					spawnedObject.transform.position = hitPose.position;
+				}
+			}
+		}
+	}
+}
