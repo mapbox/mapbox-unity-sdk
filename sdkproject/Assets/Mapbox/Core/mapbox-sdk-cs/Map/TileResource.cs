91df88dfa4eb029aade4434deacbab933d3a28c1
brnkhy <baran.kahyaoglu@mapbox.com>
2019-01-04 19:50:13 +0300

    revert uribuilder changes from before for safety

--- sdkproject/Assets/Mapbox/Core/mapbox-sdk-cs/Map/TileResource.cs
+++ sdkproject/Assets/Mapbox/Core/mapbox-sdk-cs/Map/TileResource.cs
@@ -55,7 +55,18 @@ namespace Mapbox.Map
 
 		public string GetUrl()
 		{
-			return _query;
+			var uriBuilder = new UriBuilder(_query);
+			if (uriBuilder.Query != null && uriBuilder.Query.Length > 1)
+			{
+				uriBuilder.Query = uriBuilder.Query.Substring(1) + "&" + TelemetryFactory.EventQuery;
+			}
+			else
+			{
+				uriBuilder.Query = TelemetryFactory.EventQuery;
+			}
+
+			//return uriBuilder.ToString();
+			return uriBuilder.Uri.ToString();
 		}
 	}
 }
