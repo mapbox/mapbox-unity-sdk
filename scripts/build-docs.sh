91c12a658717980511e1acf635a6c74fd96b0ef8
David Clark <dclark@mapbox.com>
2019-01-17 13:45:10 -0700

    Update site build for new publishing context
    Generates files that are necessary to publish with the latest
    version of Publisher to the docs.mapbox.com domain. Switches
    the publishable branch from mb-pages to publisher-production.
    
    Addresses #1248.

--- scripts/build-docs.sh
+++ scripts/build-docs.sh
@@ -10,3 +10,8 @@ if [ ! -d "docfx" ]; then mkdir -p docfx && unzip -o docfx.zip -d docfx; fi
 #splitting in two steps works
 mono ./docfx/docfx.exe metadata ./documentation/docfx_project/docfx.json
 mono ./docfx/docfx.exe build ./documentation/docfx_project/docfx.json
+
+#Generate files that are necessary for Publisher to deploy the right files to the right place
+echo "subdomain: docs" > ./documentation/docfx_project/_site/.publisher.yml
+echo "version: v1\ndefaults:\n- publisher" > ./documentation/docfx_project/_site/.artifacts.yml
+echo "# No content needed" > ./documentation/docfx_project/_site/_config.yml
