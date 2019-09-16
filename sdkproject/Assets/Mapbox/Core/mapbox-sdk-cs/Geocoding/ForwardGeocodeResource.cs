5c19a82dec62731e6434c48ea56340cedfa6c7f5
Abhishek Tripathi <abhishek.tripathi@mapbox.com>
2019-09-16 15:11:15 -0700

    Change to UnityWebRequest for version 2017+

--- sdkproject/Assets/Mapbox/Core/mapbox-sdk-cs/Geocoding/ForwardGeocodeResource.cs
+++ sdkproject/Assets/Mapbox/Core/mapbox-sdk-cs/Geocoding/ForwardGeocodeResource.cs
@@ -191,7 +191,11 @@ namespace Mapbox.Geocoding
 				ApiEndpoint +
 				Mode +
 #if UNITY_IOS
+#if UNITY_2017_1_OR_NEWER
+				UnityEngine.Networking.UnityWebRequest.EscapeURL(Query) +
+#else
 				WWW.EscapeURL(Query) +
+#endif
 #else
 				Uri.EscapeDataString(Query) +
 #endif
