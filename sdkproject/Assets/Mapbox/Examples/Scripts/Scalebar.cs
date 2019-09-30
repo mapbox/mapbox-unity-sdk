74285275ed7bcf56ae4b2bc008d3e65df7477bca
brnkhy <baran.kahyaoglu@mapbox.com>
2019-09-30 23:13:37 +0300

    add scalebar prefab to project and to zoomable map demo scene

---
+++ sdkproject/Assets/Mapbox/Examples/Scripts/Scalebar.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Mapbox.Unity.Map;
+using Mapbox.Unity.Utilities;
+using Mapbox.Utils;
+using UnityEngine;
+using UnityEngine.UI;
+using Constants = Mapbox.Unity.Constants;
+
+public class Scalebar : MonoBehaviour
+{
+    [SerializeField] private AbstractMap Map;
+    [SerializeField] private Transform FirstPoint;
+    [SerializeField] private Transform SecondPoint;
+    [SerializeField] private Text ScaleText;
+
+    private Camera _camera;
+    private RaycastHit _hit;
+
+    Vector2d _ll1 = new Vector2d();
+    Vector2d _ll2 = new Vector2d();
+    private Ray _ray;
+    private float _distanceLineAngle;
+    private float _distanceVectorMagnitude;
+
+    private void Start()
+    {
+        _camera = Camera.main;
+    }
+
+    void Update()
+    {
+        MidBar();
+        UpdateDistanceLabel();
+    }
+
+    private void UpdateDistanceLabel()
+    {
+        _ray = _camera.ScreenPointToRay(FirstPoint.position);
+        if (Physics.Raycast(_ray, out _hit))
+        {
+            _ll1 = Map.WorldToGeoPosition(_hit.point);
+        }
+
+        _ray = _camera.ScreenPointToRay(SecondPoint.position);
+        if (Physics.Raycast(_ray, out _hit))
+        {
+            _ll2 = Map.WorldToGeoPosition(_hit.point);
+        }
+
+        ScaleText.text = (Conversions.GeoDistance(_ll1.y, _ll1.x, _ll2.y, _ll2.x) * 1000).ToString("F2") + "m";
+    }
+
+    private void MidBar()
+    {
+        _distanceLineAngle = Vector3.SignedAngle(SecondPoint.position - FirstPoint.position, new Vector3(1, 0, 0), new Vector3(0, 0, 1));
+        _distanceVectorMagnitude = (SecondPoint.position - FirstPoint.position).magnitude / 10;
+        transform.localScale = new Vector3(_distanceVectorMagnitude, 1, 1);
+        transform.rotation = Quaternion.Euler(0, 0, -_distanceLineAngle);
+        transform.position = FirstPoint.position;
+    }
+}
---
+++ sdkproject/Assets/Mapbox/Examples/Scripts/UIElementDragger.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class UIElementDragger : EventTrigger
+{
+	private bool dragging;
+
+	public void Update()
+	{
+		if (dragging)
+		{
+			transform.position = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+		}
+	}
+
+	public override void OnPointerDown(PointerEventData eventData)
+	{
+		dragging = true;
+	}
+
+	public override void OnPointerUp(PointerEventData eventData)
+	{
+		dragging = false;
+	}
+}
