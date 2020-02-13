79664cc3329e7525344bb713fd21d167b55febcb
Baran Kahyaoğlu <baran.kahyaoglu@mapbox.com>
2020-02-13 21:49:12 +0300

    Fix flat terrain collider  (#1538)
    * remove adding mesh collider logic from set height data method as it isn't called for flat terrain
    change unity tile collider getter to add component if it's missing (changed getcomponent to addcomponent. it sets the local field directly anyway so unless another source adds the component, this should be fine)
    add collider mesh logic to flat terrain strategy (using mesh instead of box collider as mesh collider is the default one for now. can be optimized better)
    
    * update changelog
    
    * add an extra collider component check to prevent duplicate components

--- sdkproject/Assets/Mapbox/Unity/MeshGeneration/Data/UnityTile.cs
+++ sdkproject/Assets/Mapbox/Unity/MeshGeneration/Data/UnityTile.cs
@@ -73,7 +73,11 @@ namespace Mapbox.Unity.MeshGeneration.Data
 			{
 				if (_collider == null)
 				{
-					_collider = GetComponent<Collider>();
+					_collider = gameObject.GetComponent<MeshCollider>();
+					if (_collider == null)
+					{
+						_collider = gameObject.AddComponent<MeshCollider>();
+					}
 				}
 				return _collider;
 			}
@@ -268,10 +272,7 @@ namespace Mapbox.Unity.MeshGeneration.Data
 					}
 				}
 
-				if (addCollider && gameObject.GetComponent<MeshCollider>() == null)
-				{
-					gameObject.AddComponent<MeshCollider>();
-				}
+
 
 				HeightDataState = TilePropertyState.Loaded;
 			}
