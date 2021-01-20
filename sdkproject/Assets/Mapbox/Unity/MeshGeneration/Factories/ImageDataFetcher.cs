41e97622b30493922c3ddcd7fe20d9f482dfb060
Baran Kahyaoglu <brnkahyaoglu@gmail.com>
2021-01-20 16:21:03 +0300

    fix a bug where file request were called for recycled tiles, causing error in data fetcher active requests list (due to duplicate requests) move BaseImageDataFetcher to separate file

--- sdkproject/Assets/Mapbox/Unity/MeshGeneration/Factories/ImageDataFetcher.cs
+++ sdkproject/Assets/Mapbox/Unity/MeshGeneration/Factories/ImageDataFetcher.cs
@@ -22,17 +22,6 @@ public class ImageDataFetcher : DataFetcher
 
 	}
 
-	public override void FetchData(DataFetcherParameters parameters)
-	{
-		var imageDataParameters = parameters as ImageDataFetcherParameters;
-		if(imageDataParameters == null)
-		{
-			return;
-		}
-
-		FetchData(imageDataParameters.tilesetId, imageDataParameters.canonicalTileId, imageDataParameters.useRetina, imageDataParameters.tile);
-	}
-
 	public virtual void FetchData(RasterTile tile, string tilesetId, CanonicalTileId tileId, bool useRetina, UnityTile unityTile = null)
 	{
 		//MemoryCacheCheck
@@ -52,8 +41,9 @@ public class ImageDataFetcher : DataFetcher
 		{
 			MapboxAccess.Instance.CacheManager.GetTextureItemFromFile(tilesetId, tileId, (textureCacheItem) =>
 			{
-				if (unityTile != null && unityTile.CanonicalTileId != tileId)
+				if (unityTile != null && !unityTile.ContainsDataTile(tile))
 				{
+					//rasterTile.Clear();
 					//this means tile object is recycled and reused. Returned data doesn't belong to this tile but probably the previous one. So we're trashing it.
 					return;
 				}
@@ -107,91 +97,6 @@ public class ImageDataFetcher : DataFetcher
 		});
 	}
 
-	//tile here should be totally optional and used only not to have keep a dictionary in terrain factory base
-	public void FetchData(string tilesetId, CanonicalTileId tileId, bool useRetina, UnityTile unityTile = null)
-	{
-		//MemoryCacheCheck
-		var textureItem = MapboxAccess.Instance.CacheManager.GetTextureItemFromMemory(tilesetId, tileId);
-		if (textureItem != null)
-		{
-			var rasterTile = new RasterTile(tileId, tilesetId);
-			rasterTile.SetTextureFromCache(textureItem.Texture2D);
-			if (unityTile != null) { unityTile.AddTile(rasterTile); }
-
-			TextureReceived(unityTile, rasterTile);
-			return;
-		}
-
-		//FileCacheCheck
-		if (MapboxAccess.Instance.CacheManager.TextureFileExists(tilesetId, tileId)) //not in memory, check file cache
-		{
-			MapboxAccess.Instance.CacheManager.GetTextureItemFromFile(tilesetId, tileId, (textureCacheItem) =>
-			{
-				if (unityTile != null && unityTile.CanonicalTileId != tileId)
-				{
-					//this means tile object is recycled and reused. Returned data doesn't belong to this tile but probably the previous one. So we're trashing it.
-					return;
-				}
-
-				//even though we just checked file exists, system couldn't find&load it
-				//this shouldn't happen frequently, only in some corner cases
-				//one possibility might be file being pruned due to hitting cache limit
-				//after that first check few lines above and actual loading (loading is scheduled and delayed so it's not in same frame)
-				if (textureCacheItem != null)
-				{
-					var rasterTile = new RasterTile(tileId, tilesetId);
-					rasterTile.SetTextureFromCache(textureCacheItem.Texture2D);
-					rasterTile.ETag = textureCacheItem.ETag;
-					rasterTile.ExpirationDate = textureCacheItem.ExpirationDate.Value;
-					if (unityTile != null) { unityTile.AddTile(rasterTile); }
-
-					TextureReceived(unityTile, rasterTile);
-
-					//after returning what we already have
-					//check if it's out of date, if so check server for update
-					if (textureCacheItem.ExpirationDate < DateTime.Now)
-					{
-						CreateWebRequest(tilesetId, tileId, useRetina, textureCacheItem.ETag, unityTile);
-					}
-				}
-				else
-				{
-					CreateWebRequest(tilesetId, tileId, useRetina, String.Empty, unityTile);
-				}
-			});
-
-			return;
-		}
-
-		//not in cache so web request
-		CreateWebRequest(tilesetId, tileId, useRetina, String.Empty, unityTile);
-	}
-
-	protected virtual void CreateWebRequest(string tilesetId, CanonicalTileId tileId, bool useRetina, string etag, UnityTile unityTile = null)
-	{
-		RasterTile rasterTile;
-		//`starts with` is weak and string operations are slow
-		//but caching type and using Activator.CreateInstance (or caching func and calling it)  is even slower
-		if (tilesetId.StartsWith("mapbox://", StringComparison.Ordinal))
-		{
-			rasterTile = useRetina ? new RetinaRasterTile(tileId, tilesetId) : new RasterTile(tileId, tilesetId);
-		}
-		else
-		{
-			rasterTile = useRetina ? new ClassicRetinaRasterTile(tileId, tilesetId) : new ClassicRasterTile(tileId, tilesetId);
-		}
-
-		if (unityTile != null)
-		{
-			unityTile.AddTile(rasterTile);
-		}
-
-		EnqueueForFetching(new FetchInfo(tileId, tilesetId, rasterTile, etag)
-		{
-			Callback = () => { FetchingCallback(tileId, rasterTile, unityTile); }
-		});
-	}
-
 	protected virtual void FetchingCallback(CanonicalTileId tileId, RasterTile rasterTile, UnityTile unityTile = null)
 	{
 		if (unityTile != null && !unityTile.ContainsDataTile(rasterTile))
@@ -236,85 +141,6 @@ public class ImageDataFetcher : DataFetcher
 				TextureReceived(unityTile, rasterTile);
 			}
 		}
-	}
-}
-
-public class BaseImageDataFetcher : ImageDataFetcher
-{
-	public BaseImageDataFetcher(IFileSource fileSource) : base(fileSource)
-	{
-
-	}
-
-	public void FetchData(RasterTile tile, string tilesetId, CanonicalTileId tileId, bool useRetina, UnityTile unityTile = null)
-	{
-		//MemoryCacheCheck
-		var textureItem = MapboxAccess.Instance.CacheManager.GetTextureItemFromMemory(tilesetId, tileId);
-		if (textureItem != null)
-		{
-			var rasterTile = new RasterTile(tileId, tilesetId);
-			rasterTile.SetTextureFromCache(textureItem.Texture2D);
-#if UNITY_EDITOR
-			rasterTile.FromCache = CacheType.MemoryCache;
-#endif
-			TextureReceived(unityTile, rasterTile);
-			return;
-		}
-
-		//FileCacheCheck
-		if (MapboxAccess.Instance.CacheManager.TextureFileExists(tilesetId, tileId)) //not in memory, check file cache
-		{
-			MapboxAccess.Instance.CacheManager.GetTextureItemFromFile(tilesetId, tileId, (textureCacheItem) =>
-			{
-				//even though we just checked file exists, system couldn't find&load it
-				//this shouldn't happen frequently, only in some corner cases
-				//one possibility might be file being pruned due to hitting cache limit
-				//after that first check few lines above and actual loading (loading is scheduled and delayed so it's not in same frame)
-				if (textureCacheItem != null)
-				{
-					var rasterTile = new RasterTile(tileId, tilesetId);
-					rasterTile.SetTextureFromCache(textureCacheItem.Texture2D);
-#if UNITY_EDITOR
-					rasterTile.FromCache = CacheType.FileCache;
-#endif
-					rasterTile.ETag = textureCacheItem.ETag;
-					rasterTile.ExpirationDate = textureCacheItem.ExpirationDate.Value;
-					TextureReceived(unityTile, rasterTile);
-					MapboxAccess.Instance.CacheManager.MarkFixed(tileId, tilesetId);
-
-					//after returning what we already have
-					//check if it's out of date, if so check server for update
-					if (textureCacheItem.ExpirationDate < DateTime.Now)
-					{
-						CreateWebRequest(tilesetId, tileId, useRetina, textureCacheItem.ETag, unityTile);
-					}
-				}
-				else
-				{
-					CreateWebRequest(tilesetId, tileId, useRetina, String.Empty, unityTile);
-				}
-			});
-
-			return;
-		}
-
-		//not in cache so web request
-		EnqueueForFetching(new FetchInfo(tileId, tilesetId, tile, String.Empty)
-		{
-			Callback = () => { FetchingCallback(tileId, tile, unityTile); }
-		});
-	}
-
-	protected override void FetchingCallback(CanonicalTileId tileId, RasterTile rasterTile, UnityTile unityTile = null)
-	{
-		base.FetchingCallback(tileId, rasterTile, unityTile);
-#if UNITY_EDITOR
-		if (rasterTile.Texture2D != null)
-		{
-			rasterTile.Texture2D.name += "_fallbackImage";
-		}
-#endif
-		MapboxAccess.Instance.CacheManager.MarkFixed(rasterTile.Id, rasterTile.TilesetId);
 	}
 }
 
