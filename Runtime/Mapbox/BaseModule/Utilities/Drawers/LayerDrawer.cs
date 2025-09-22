1ec83bec3c9682ee95dcaf9a7638ea04dbd587ca
tapycha <tapycha@gmail.com>
2025-09-22 20:49:06 +0300

    Wrapped LayerDrawer in preprocessor directives #if to not get errors when compile

--- Runtime/Mapbox/BaseModule/Utilities/Drawers/LayerDrawer.cs
+++ Runtime/Mapbox/BaseModule/Utilities/Drawers/LayerDrawer.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿#if UNITY_EDITOR
+using System;
 using Mapbox.BaseModule.Utilities.Attributes;
 using UnityEditor;
 using UnityEngine;
@@ -71,3 +72,4 @@ namespace Mapbox.BaseModule.Utilities.Drawers
         }
     }
 }
+#endif
